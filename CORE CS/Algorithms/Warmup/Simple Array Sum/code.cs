85312f1c6533d5dd4b81b42c7d42bbe0e25a644e
Marco <domandominguez@hotmail.com>
2017-09-14 20:36:56 -0700

    Create code.cs

---
+++ CORE CS/Algorithms/Warmup/Simple Array Sum/code.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int simpleArraySum(int n, int[] ar) {
+        // Complete this function
+        return ar.Sum();
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        int[] ar = Array.ConvertAll(ar_temp,Int32.Parse);
+        int result = simpleArraySum(n, ar);
+        Console.WriteLine(result);
+    }
+}
