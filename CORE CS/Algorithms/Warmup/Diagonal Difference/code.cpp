d1df18f40524a4038a6567f001a72e6abd8b106d
Marco <domandominguez@hotmail.com>
2017-09-15 20:30:46 -0700

    Create code.cpp

---
+++ CORE CS/Algorithms/Warmup/Diagonal Difference/code.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector< vector<int> > a(n,vector<int>(n));
+    for(int a_i = 0;a_i < n;a_i++){
+       for(int a_j = 0;a_j < n;a_j++){
+          cin >> a[a_i][a_j];
+       }
+    }
+    int d1 = 0;
+    int d2 = 0;
+    for(int i=0;i<n;i++)
+        d1+=a[i][i];
+    for(int i=0, j = n-1; i<n;i++,j--)
+        d2+=a[i][j];
+    cout << abs(d1-d2);
+    return 0;
+}
