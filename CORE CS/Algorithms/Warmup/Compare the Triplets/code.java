aa4e801a70fe5dfbcf07b1a8822b1779f28b3333
Marco <domandominguez@hotmail.com>
2017-09-15 17:48:39 -0700

    Create code.java

---
+++ CORE CS/Algorithms/Warmup/Compare the Triplets/code.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+        int[] r = new int[]{0,0};
+        if(a0 > b0) r[0]++;
+        else if(a0 < b0) r[1]++;
+        if(a1 > b1) r[0]++;
+        else if(a1 < b1) r[1]++;
+        if(a2 > b2) r[0]++;
+        else if(a2 < b2) r[1]++;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
