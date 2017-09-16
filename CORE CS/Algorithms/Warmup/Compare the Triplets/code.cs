dcfe4ed3f3324e934525975237cf4da1ba156929
Marco <domandominguez@hotmail.com>
2017-09-15 17:46:19 -0700

    Create code.cs

---
+++ CORE CS/Algorithms/Warmup/Compare the Triplets/code.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+        int[] r = new int[2]{0,0};
+         if(a0 > b0) r[0]++;
+        else if(a0 < b0) r[1]++;
+        if(a1 > b1) r[0]++;
+        else if(a1 < b1) r[1]++;
+        if(a2 > b2) r[0]++;
+        else if(a2 < b2) r[1]++;
+        return r;
+    }
+
+    static void Main(String[] args) {
+        string[] tokens_a0 = Console.ReadLine().Split(' ');
+        int a0 = Convert.ToInt32(tokens_a0[0]);
+        int a1 = Convert.ToInt32(tokens_a0[1]);
+        int a2 = Convert.ToInt32(tokens_a0[2]);
+        string[] tokens_b0 = Console.ReadLine().Split(' ');
+        int b0 = Convert.ToInt32(tokens_b0[0]);
+        int b1 = Convert.ToInt32(tokens_b0[1]);
+        int b2 = Convert.ToInt32(tokens_b0[2]);
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        Console.WriteLine(String.Join(" ", result));
+        
+
+    }
+}
