6f52e7a7a397269455e39bda025d1da86f8a0822
Marco <domandominguez@hotmail.com>
2017-09-17 16:02:47 -0700

    Create code.cs

---
+++ CORE CS/Algorithms/Warmup/Birthday Cake Candles/code.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        Array.Sort(ar);
+        return ar.Where(x => x == ar.Last()).Count();
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        int[] ar = Array.ConvertAll(ar_temp,Int32.Parse);
+        int result = birthdayCakeCandles(n, ar);
+        Console.WriteLine(result);
+    }
+}
