f5ae5e81c9efbd6749d143862bce7914227225b4
Marco <domandominguez@hotmail.com>
2017-09-17 22:45:10 -0700

    Create code1.cs

---
+++ CORE CS/Algorithms/Warmup/Time Conversion/code1.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        Console.WriteLine(DateTime.Parse(Console.ReadLine()).ToString("HH:mm:ss"));
+    }
+}
