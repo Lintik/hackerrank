2cd0980abe08e873decd243ea2868c1715de3b6f
Marco <domandominguez@hotmail.com>
2017-09-17 22:45:38 -0700

    Update code.cpp

--- CORE CS/Algorithms/Warmup/Time Conversion/code.cpp
+++ CORE CS/Algorithms/Warmup/Time Conversion/code.cpp
@@ -1,1 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    string h;
+    int hr;
+    cin>>s;
+    hr = ((s[0]-'0')*10)+(s[1]-'0');
+    if(s[8]=='P'&&s[9]=='M'&& hr ==12) cout<<to_string(hr);
+    else if(s[8]=='P'&&s[9]=='M') cout<<to_string(hr+12);
+    else if(s[8]=='A'&&s[9]=='M'&&hr==12) cout<<"00";
+    
+    else cout<< s[0]<<s[1];
+    
+   
+    for(int i =2;i<8;i++)
+        cout<<s[i];
+    cout<<endl;
+    return 0;
+}
