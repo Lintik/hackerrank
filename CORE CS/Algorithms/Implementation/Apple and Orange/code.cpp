c94eefce6a2b59bcd7d7f025eba20f2e11d10f6b
Marco <domandominguez@hotmail.com>
2017-09-19 18:28:23 -0700

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Apple and Orange/code.cpp
+++ CORE CS/Algorithms/Implementation/Apple and Orange/code.cpp
@@ -1,1 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, m, n, s, t;
+    vector<int> apple(m);
+    vector<int> orange(m);
+    cin >> s >> t;
+    cin >> a >> b;
+    cin >> m >> n;
+    for(int apple_i = 0;apple_i < m;apple_i++){
+       cin >> apple[apple_i];
+    }
+    for(int orange_i = 0;orange_i < n;orange_i++){
+       cin >> orange[orange_i];
+    }
+    
+    
+    return 0;
+}
