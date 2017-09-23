8f126d6f92b2a6fed143cb8747995ebcf79f469b
Marco <domandominguez@hotmail.com>
2017-09-23 16:17:33 -0700

    Update code.java

--- Languages/Java/Introduction/Java End-of-file/code.java
+++ Languages/Java/Introduction/Java End-of-file/code.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        for(int i=1;sc.hasNextLine();i++){
+            System.out.print(i);
+            System.out.print(' ');
+            System.out.println(sc.nextLine());
+        }
+    }
+}
