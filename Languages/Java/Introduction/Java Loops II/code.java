59a2b9ab00267d33a5e0e9c1e25e23c1e33d2557
Marco <domandominguez@hotmail.com>
2017-09-23 15:25:08 -0700

    Update code.java

--- Languages/Java/Introduction/Java Loops II/code.java
+++ Languages/Java/Introduction/Java Loops II/code.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import java.io.*;
 
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int m = a;
+            for(int j=0;j<n;j++){
+                m += (Math.pow(2,j) * b);
+                System.out.print(m);
+                System.out.print(' ');
+            }
+            System.out.println();
+            
+        }
+        in.close();
+    }
+}
