cc72466a66019b0bab06165d2f82acb7ebc08e91
Marco <domandominguez@hotmail.com>
2017-09-23 14:35:56 -0700

    Update code.java

--- Languages/Java/Introduction/Java Output Formatting/code.java
+++ Languages/Java/Introduction/Java Output Formatting/code.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                
+                System.out.print(String.format("%-15s", s1 ));
+                System.out.println(String.format("%-15s", String.format("%3s", x).replace(' ', '0')));
+            }
+            System.out.println("================================");
+
+    }
+}
