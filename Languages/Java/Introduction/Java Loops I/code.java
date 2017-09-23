f8ebd0b5f5ce6e6e721d19285042d1613b286309
Marco <domandominguez@hotmail.com>
2017-09-23 14:51:28 -0700

    Update code.java

--- Languages/Java/Introduction/Java Loops I/code.java
+++ Languages/Java/Introduction/Java Loops I/code.java
@@ -1,1 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1;i<11;i++){
+            String t = String.format("2 x %s = %s", String.valueOf(i), String.valueOf(N*i));
+            System.out.println(t);
+        }
+    }
+}
