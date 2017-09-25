8b05e47bb4556d05ef1221f41544262c28157dca
Marco <domandominguez@hotmail.com>
2017-09-24 21:35:51 -0700

    Update code.java

--- Languages/Java/Strings/Java String Tokens/code.java
+++ Languages/Java/Strings/Java String Tokens/code.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.trim();
+        if(s.length() == 0){
+            System.out.println("0");
+            return;
+        }
+        String[] ss=s.split("[ !,?._'@]+");
+        System.out.println(ss.length);
+        for(int i = 0;i<ss.length;i++)
+            System.out.println(ss[i]);
+        scan.close();
+    }
+}
