b78871c7b78f9655bba890b739313c301a04faae
Marco <domandominguez@hotmail.com>
2017-09-24 19:13:54 -0700

    Update code.java

--- Languages/Java/Strings/Java String Reverse/code.java
+++ Languages/Java/Strings/Java String Reverse/code.java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int l = A.length();
+        for(int i=0;i<l;i++){
+            if(A.charAt(i) != A.charAt(l-1-i)){
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+        
+    }
+}
