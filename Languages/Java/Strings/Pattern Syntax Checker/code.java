af49ea13442ebbbce1fcc6ed7789235d3bc06473
Marco <domandominguez@hotmail.com>
2017-09-24 21:43:34 -0700

    Update code.java

--- Languages/Java/Strings/Pattern Syntax Checker/code.java
+++ Languages/Java/Strings/Pattern Syntax Checker/code.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.*;
 
+public class Solution
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+          
+        for(int i=0;i<testCases;i++){
+            try{
+                String pattern = in.nextLine();
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }
+            catch (Exception e){
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
