274449f5bdd833e74fd42eb569ae3974ee3ecb79
Marco <domandominguez@hotmail.com>
2017-09-24 20:19:09 -0700

    Update code.java

--- Languages/Java/Strings/Java Anagrams/code.java
+++ Languages/Java/Strings/Java Anagrams/code.java
@@ -1,1 +1,28 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+   static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        char[] charsA = a.toCharArray();
+        char[] charsB = b.toCharArray();
+        Arrays.sort(charsA);
+        Arrays.sort(charsB);
+        
+        String sortedA = new String(charsA);
+        String sortedB = new String(charsB);
+     
+        return sortedA.equals(sortedB);
+    }
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
