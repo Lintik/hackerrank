7f633203dcd18baccf6df2d4666d8e6730afcf01
Marco <domandominguez@hotmail.com>
2017-09-28 22:13:06 -0700

    Update code.java

--- Languages/Java/Introduction/Java Date and Time/code.java
+++ Languages/Java/Introduction/Java Date and Time/code.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+import java.time.LocalDate;
 
+public class Solution {
+    public static String getDay(String day, String month, String year) {
+        /*
+        * Write your code here.
+        */
+        LocalDate dt = LocalDate.of(Integer.valueOf(year), Integer.valueOf(month), Integer.valueOf(day));
+        return dt.getDayOfWeek().toString();    
+    }
+  
+  public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+        
+        System.out.println(getDay(day, month, year));
+    }
+}
