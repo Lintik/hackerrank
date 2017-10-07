ed5c6923acd02282e6564947d8414c70d0ac2e40
Marco <domandominguez@hotmail.com>
2017-10-07 15:07:47 -0700

    Update code.cpp

--- CORE CS/Algorithms/Warmup/Staircase/code.cpp
+++ CORE CS/Algorithms/Warmup/Staircase/code.cpp
@@ -1,1 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n = 0;
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        for(int j = n;j>i+1;j--)
+            cout<<' ';
+        for(int k = i; k>-1;k--)
+            cout<<'#';
+        
+        cout<<endl;
+    }
+    return 0;
+}
