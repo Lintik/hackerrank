de30c7763cd1f2bb65eb139fb49e04da2554537c
Marco <domandominguez@hotmail.com>
2017-10-07 16:06:14 -0700

    Delete code1.cs

--- CORE CS/Algorithms/Warmup/Time Conversion/code1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-class Solution {
-    static void Main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        Console.WriteLine(DateTime.Parse(Console.ReadLine()).ToString("HH:mm:ss"));
-    }
-}
