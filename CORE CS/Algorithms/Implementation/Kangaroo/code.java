20f622029a8e0c74ef58359ea01589344fe8cdc7
Marco <domandominguez@hotmail.com>
2017-10-08 21:10:57 -0700

    Update code.java

--- CORE CS/Algorithms/Implementation/Kangaroo/code.java
+++ CORE CS/Algorithms/Implementation/Kangaroo/code.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 public class Solution {
 
     static String kangaroo(int x1, int v1, int x2, int v2) {
-        if(v2 > v1) return "NO";
+        if(v2 >= v1) return "NO";
         while(x1 < x2){
             x1+=v1;
             x2+=v2;
