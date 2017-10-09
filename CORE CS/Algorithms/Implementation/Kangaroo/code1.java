229cc88f80565398d5d4ad1539a1e39af1065ac8
Marco <domandominguez@hotmail.com>
2017-10-08 21:12:12 -0700

    Create code1.java

---
+++ CORE CS/Algorithms/Implementation/Kangaroo/code1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(v2 >= v1) return "NO";
+        return (x2-x1)%(v1-v2) == 0? "YES":"NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+}
