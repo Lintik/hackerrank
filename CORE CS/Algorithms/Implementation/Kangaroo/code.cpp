2e3e8c496b90d468812d1dd28e7ef0eb8c47d84d
Marco <domandominguez@hotmail.com>
2017-10-08 18:35:16 -0700

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Kangaroo/code.cpp
+++ CORE CS/Algorithms/Implementation/Kangaroo/code.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+    if(v2 > v1) return "NO";
+    while(x1 < x2){
+        x1+=v1;
+        x2+=v2;
+    }
+    if(x1==x2) return "YES";
+    else return "NO";
+}
+
+int main() {
+    int x1;
+    int v1;
+    int x2;
+    int v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    string result = kangaroo(x1, v1, x2, v2);
+    cout << result << endl;
+    return 0;
+}
