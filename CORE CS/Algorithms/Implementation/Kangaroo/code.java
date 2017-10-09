5349daa1f0ac5c7b2a53bf19bb9252c53a1d96c1
Marco <domandominguez@hotmail.com>
2017-10-08 20:34:43 -0700

    Create code.java

---
+++ CORE CS/Algorithms/Implementation/Kangaroo/code.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(v2 > v1) return "NO";
+        while(x1 < x2){
+            x1+=v1;
+            x2+=v2;
+        }
+        if(x1==x2) return "YES";
+        else return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+}
