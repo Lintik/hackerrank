f9060cc996b363a70c2a9189018f3816b8d90d30
Marco <domandominguez@hotmail.com>
2017-10-08 21:10:02 -0700

    Update code1.cpp

--- CORE CS/Algorithms/Implementation/Kangaroo/code1.cpp
+++ CORE CS/Algorithms/Implementation/Kangaroo/code1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 string kangaroo(int x1, int v1, int x2, int v2) {
-    if(v2 > v1) return "NO";
+    if(v2 >= v1) return "NO";
     return (x2-x1)%(v1-v2) == 0? "YES":"NO";
 }
 
