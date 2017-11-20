b074aca41be4334b8daddd38f449c344ec93c296
Marco <domandominguez@hotmail.com>
2017-11-19 18:40:00 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Breaking the Records/code.cpp
+++ CORE CS/Algorithms/Implementation/Breaking the Records/code.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+vector < int > getRecord(vector < int > s){
+    // Complete this function
+    int maxc = 0,
+        minc = 0,
+        max = s[0],
+        min = s[0];
+        
+    for(int i=1;i<s.size();i++){
+        if(max < s[i]){
+            max = s[i];
+            maxc++;
+        }
+        else if(min > s[i]){
+            min = s[i];
+            minc++;
+        }
+    }
+    vector<int> r;
+    r.push_back(maxc);
+    r.push_back(minc);
+    return r;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> s(n);
+    for(int s_i = 0; s_i < n; s_i++){
+       cin >> s[s_i];
+    }
+    vector < int > result = getRecord(s);
+    string separator = "", delimiter = " ";
+    for(auto val: result) {
+        cout<<separator<<val;
+        separator = delimiter;
+    }
+    cout<<endl;
+    return 0;
+}
