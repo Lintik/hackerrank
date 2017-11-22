f5d066c8bdc8bfec741b0d239062bd6bda85abe2
Marco <domandominguez@hotmail.com>
2017-11-22 15:50:13 -0800

    Create code.java

---
+++ CORE CS/Algorithms/Implementation/Birthday Chocolate/code.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int[] s, int d, int m){
+        // Complete this function
+        int t=0,
+        c=0;
+        
+        for(int i=0;i<m;i++) t+=s[i];
+        if(t==d) c++;
+        
+        for(int i=m;i<n;i++){
+            t = t - s[i-m] + s[i];
+            if(t==d) c++;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = solve(n, s, d, m);
+        System.out.println(result);
+    }
+}
