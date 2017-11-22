3f0ea696ba670ebf19c24f1407117e88da16cf39
Marco <domandominguez@hotmail.com>
2017-11-22 15:49:31 -0800

    Create code.cs

---
+++ CORE CS/Algorithms/Implementation/Birthday Chocolate/code.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int solve(int n, int[] s, int d, int m){
+        // Complete this function
+        int t=0,
+        c=0;
+        
+        for(int i=0;i<m;i++) t+=s[i];
+        if(t==d) c++;
+        
+        for(int i=m;i<n;i++){
+            t = t - s[i-m] + s[i];
+            if(t==d) c++;
+        }
+        return c;
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] s_temp = Console.ReadLine().Split(' ');
+        int[] s = Array.ConvertAll(s_temp,Int32.Parse);
+        string[] tokens_d = Console.ReadLine().Split(' ');
+        int d = Convert.ToInt32(tokens_d[0]);
+        int m = Convert.ToInt32(tokens_d[1]);
+        int result = solve(n, s, d, m);
+        Console.WriteLine(result);
+    }
+}
