50ba9277f5447c2a7dfd0cf54deb52adf16c0f30
Marco <domandominguez@hotmail.com>
2017-11-22 15:48:02 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Birthday Chocolate/code.cpp
+++ CORE CS/Algorithms/Implementation/Birthday Chocolate/code.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int solve(int n, vector < int > s, int d, int m){
+    // Complete this function
+    int t=0,
+        c=0;
+    for(int i=0;i<m;i++){
+        t+=s[i];
+    }
+    if(t==d) c++;
+    for(int i=m;i<n;i++){
+        t = t - s[i-m] + s[i];
+        if(t==d) c++;
+    }
+    return c;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> s(n);
+    for(int s_i = 0; s_i < n; s_i++){
+       cin >> s[s_i];
+    }
+    int d;
+    int m;
+    cin >> d >> m;
+    int result = solve(n, s, d, m);
+    cout << result << endl;
+    return 0;
+}
