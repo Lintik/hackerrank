4802b07154558fc3f98366681719d158643cc1ab
Marco <domandominguez@hotmail.com>
2017-11-28 16:26:35 -0800

    Create code.cs

---
+++ CORE CS/Algorithms/Implementation/Disivible Sum Pairs/code.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        // Complete this function
+        int c=0;
+        for(int i=0;i<n-1;i++)
+            for(int j=i+1;j<n;j++)
+                if((ar[i]+ar[j]) % k == 0) c++;
+        return c;
+    }
+
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] ar_temp = Console.ReadLine().Split(' ');
+        int[] ar = Array.ConvertAll(ar_temp,Int32.Parse);
+        int result = divisibleSumPairs(n, k, ar);
+        Console.WriteLine(result);
+    }
+}
