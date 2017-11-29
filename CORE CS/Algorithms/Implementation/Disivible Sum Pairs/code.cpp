536581f35f9a4ee789c7addf8de7635871db17ef
Marco <domandominguez@hotmail.com>
2017-11-28 16:25:07 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Disivible Sum Pairs/code.cpp
+++ CORE CS/Algorithms/Implementation/Disivible Sum Pairs/code.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int divisibleSumPairs(int n, int k, vector <int> ar) {
+    // Complete this function
+    int c=0;
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if((ar[i]+ar[j]) % k == 0) c++;
+    return c;
+}
+
+int main() {
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = divisibleSumPairs(n, k, ar);
+    cout << result << endl;
+    return 0;
+}
