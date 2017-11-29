108e7c05d33c31467f764d93d3f3342f0d47b470
Marco <domandominguez@hotmail.com>
2017-11-28 17:33:04 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Migratory Birds/code.cpp
+++ CORE CS/Algorithms/Implementation/Migratory Birds/code.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int migratoryBirds(int n, vector <int> ar) {
+    // Complete this function
+    sort(ar.begin(),ar.end());
+    
+    int previous = ar[0];
+    int popular = ar[0];
+    int count = 1;
+    int maxCount = 1;
+
+    for (int i = 1; i < ar.size(); i++) {
+        if (ar[i] == previous)
+            count++;
+        else {
+            if (count > maxCount) {
+                popular = ar[i-1];
+                maxCount = count;
+            }
+            previous = ar[i];
+            count = 1;
+        }
+    }
+
+    return count > maxCount ? ar[ar.size()-1] : popular;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = migratoryBirds(n, ar);
+    cout << result << endl;
+    return 0;
+}
