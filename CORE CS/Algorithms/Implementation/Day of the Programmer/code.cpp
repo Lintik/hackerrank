e544dfbbc84d23af0444b87b289fe56582a993ab
Marco <domandominguez@hotmail.com>
2017-11-29 18:41:22 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Day of the Programmer/code.cpp
+++ CORE CS/Algorithms/Implementation/Day of the Programmer/code.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string solve(int year){
+    // Complete this function
+    int mon7 = 215;
+    short int feb; //days in february
+    if(year < 1918) //julian
+        feb = year%4 ? 28 : 29;
+    else if(year > 1918) //gregorian
+        feb = !(year%400) || year%100&&!(year%4) ? 29 : 28;
+    else // 1918
+        feb = 15;
+    feb = 256 - (feb + mon7);
+    return to_string(feb) + ".09." + to_string(year);
+}
+
+int main() {
+    int year;
+    cin >> year;
+    string result = solve(year);
+    cout << result << endl;
+    return 0;
+}
