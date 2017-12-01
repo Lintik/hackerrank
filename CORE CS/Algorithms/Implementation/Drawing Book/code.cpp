f0d1ad070c2c851814409b5d4b2601fbff05d1fb
Marco <domandominguez@hotmail.com>
2017-11-30 21:32:25 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Drawing Book/code.cpp
+++ CORE CS/Algorithms/Implementation/Drawing Book/code.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int solve(int n, int p){
+    // Complete this function
+    return min(p/2,n/2-p/2);
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int p;
+    cin >> p;
+    int result = solve(n, p);
+    cout << result << endl;
+    return 0;
+}
