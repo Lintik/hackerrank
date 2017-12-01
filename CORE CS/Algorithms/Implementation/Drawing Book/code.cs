39fb6601902dd4dc7f1edd7f3fd29d6a27ab8206
Marco <domandominguez@hotmail.com>
2017-11-30 21:34:26 -0800

    Create code.cs

---
+++ CORE CS/Algorithms/Implementation/Drawing Book/code.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int solve(int n, int p){
+        // Complete this function
+        return Math.Min(p/2,n/2-p/2);
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int p = Convert.ToInt32(Console.ReadLine());
+        int result = solve(n, p);
+        Console.WriteLine(result);
+    }
+}
