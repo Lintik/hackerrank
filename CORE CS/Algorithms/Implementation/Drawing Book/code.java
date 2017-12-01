9d4646e1f09877e9f458219c6abd446f24dd64f9
Marco <domandominguez@hotmail.com>
2017-11-30 21:33:52 -0800

    Create code.java

---
+++ CORE CS/Algorithms/Implementation/Drawing Book/code.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int p){
+        // Complete this function
+        return Math.min(p/2,n/2-p/2);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = in.nextInt();
+        int result = solve(n, p);
+        System.out.println(result);
+    }
+}
