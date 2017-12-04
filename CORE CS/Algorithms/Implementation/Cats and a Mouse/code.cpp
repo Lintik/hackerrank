a6f4670a1bb191a176b086e6ceb01d66b54278b8
Marco <domandominguez@hotmail.com>
2017-12-04 06:35:38 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Cats and a Mouse/code.cpp
+++ CORE CS/Algorithms/Implementation/Cats and a Mouse/code.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int x;
+        int y;
+        int z;
+        cin >> x >> y >> z;
+        
+        int xz = abs(x - z);
+        int yz = abs(y - z);
+        if(xz > yz) cout << "Cat B";
+        else if(xz < yz) cout << "Cat A";
+        else cout << "Mouse C";
+        cout << endl;
+    }
+    
+    return 0;
+}
