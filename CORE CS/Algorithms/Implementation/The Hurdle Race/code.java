cfc2a67fcc90f4b2abf5461bbfed4bad6e71525e
Marco <domandominguez@hotmail.com>
2017-12-11 21:35:11 -0800

    Create code.java

---
+++ CORE CS/Algorithms/Implementation/The Hurdle Race/code.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        int m = 0;
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+            if(height[height_i] > m) m = height[height_i];
+        }
+        if(m > k) System.out.println(m - k);
+        else System.out.println(0);
+        in.close();
+    }
+}
