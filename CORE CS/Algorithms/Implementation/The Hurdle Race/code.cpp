67e9dcce44315743a36472404e36cd4d1ad6f74b
Marco <domandominguez@hotmail.com>
2017-12-11 21:08:04 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/The Hurdle Race/code.cpp
+++ CORE CS/Algorithms/Implementation/The Hurdle Race/code.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int p;
+    cin >> p;
+    vector<int> a(n);
+    int m = 0;
+    for(int i = 0; i < n;i++){
+        cin >> a[i];
+        if(a[i] > m) m = a[i];
+    }
+    if(m > p)
+        cout << m - p;
+    else
+        cout << 0;
+    return 0;
+    
+}
