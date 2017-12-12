3abea6c6e465152ef9233f874d7b74cc5e070a41
Marco <domandominguez@hotmail.com>
2017-12-12 13:41:07 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Utopian Tree/code.java
+++ CORE CS/Algorithms/Implementation/Utopian Tree/code.java
@@ -1,1 +1,15 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t;i++){
+            int n = sc.nextInt();
+            int m = ~(~1<<(n>>1)) << n%2;
+            System.out.println(m);
+        }
+    }
+}
