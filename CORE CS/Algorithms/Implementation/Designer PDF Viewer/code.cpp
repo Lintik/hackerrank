847ed9a9b5a213f494039b9f3e14d0143913fe48
Marco <domandominguez@hotmail.com>
2017-12-12 02:16:42 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.cpp
+++ CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    vector<int> h(26);
+    for(int i = 0; i < 26; i++)
+        cin >> h[i];
+    string word;
+    cin >> word;
+    int m = 0;
+    for(char letter: word){
+        if(h[letter - 'a'] > m)
+            m = h[letter - 'a'];
+    }
+    cout << word.length() * m;
+    return 0;
+}
