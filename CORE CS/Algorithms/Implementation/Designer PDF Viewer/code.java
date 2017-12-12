4cdae7430fde4d11eb7820655d1993fb7adc8967
Marco <domandominguez@hotmail.com>
2017-12-12 12:39:39 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.java
+++ CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] h = new int[26];
+        for(int h_i=0; h_i < 26; h_i++){
+            h[h_i] = in.nextInt();
+        }
+        String word = in.next();
+        int m = 0;
+        for(int i = 0; i < word.length();i++){
+            if(h[word.charAt(i) - 'a'] > m)
+                m = h[word.charAt(i) - 'a'];
+        }
+        System.out.println(word.length() * m);
+    }
+}
