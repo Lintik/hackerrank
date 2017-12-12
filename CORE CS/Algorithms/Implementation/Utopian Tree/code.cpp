d98968cf62c039cae2ac15c0876238728ce97acf
Marco <domandominguez@hotmail.com>
2017-12-12 13:08:22 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Utopian Tree/code.cpp
+++ CORE CS/Algorithms/Implementation/Utopian Tree/code.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        int m = ~(~1<<(a[i]>>1)) << a[i]%2;
+        cout << m << endl;
+    }
+    return 0;
+}
