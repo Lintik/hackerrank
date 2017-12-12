9175b675aa13e3c56de259a3db0dd6eb5b26be5f
Marco <domandominguez@hotmail.com>
2017-12-12 12:43:13 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.cs
+++ CORE CS/Algorithms/Implementation/Designer PDF Viewer/code.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
+    static void Main(String[] args) {
+        string[] h_temp = Console.ReadLine().Split(' ');
+        int[] h = Array.ConvertAll(h_temp,Int32.Parse);
+        string word = Console.ReadLine();
+        int m = 0;
+        for(int i = 0; i < word.Length;i++){
+            if(h[word[i] - 'a'] > m)
+                m = h[word[i] - 'a'];
+        }
+        Console.WriteLine(m * word.Length);
+    }
+}
