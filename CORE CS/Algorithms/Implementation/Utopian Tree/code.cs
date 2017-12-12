1bdb6a42a03cac988791d681bfe73241c52793f9
Marco <domandominguez@hotmail.com>
2017-12-12 13:26:22 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Utopian Tree/code.cs
+++ CORE CS/Algorithms/Implementation/Utopian Tree/code.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = ~(~1<<(n>>1)) << n%2;
+            Console.WriteLine(m);
+        }
+    }
+}
