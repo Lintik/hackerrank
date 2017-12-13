684326869db453c17ce9240054d2ec53923d2c5d
Marco <domandominguez@hotmail.com>
2017-12-12 23:26:13 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Viral Advertising/code.cpp
+++ CORE CS/Algorithms/Implementation/Viral Advertising/code.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> m(n+1);
+    m[0] = 2;
+    for(int i = 0; i < n - 1;i++)
+        m[i+1] = 3*m[i]/2;
+    int t = 0;
+    for(int i: m)
+        t += i;
+    cout << t;
+    return 0;
+}
