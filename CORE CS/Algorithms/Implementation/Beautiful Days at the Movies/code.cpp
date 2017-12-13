be035c3281b4e9d2a0ce4bcc9a44aa62cef55d14
Marco <domandominguez@hotmail.com>
2017-12-12 20:31:55 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Beautiful Days at the Movies/code.cpp
+++ CORE CS/Algorithms/Implementation/Beautiful Days at the Movies/code.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int m;
+    cin >> m;
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int c = 0;
+    for(int i = m; i <= n;i++){
+        int r = 0;
+        int temp = i;
+        while(temp){
+            r *= 10;
+            r += temp % 10;
+            temp /= 10;
+        }
+        if(abs(i - r) % k == 0) c++;
+    }
+    cout << c;
+}
