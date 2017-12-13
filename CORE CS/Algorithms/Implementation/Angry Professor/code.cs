85058f7af3af98e5fe53b9e50e1b005ea633a605
Marco <domandominguez@hotmail.com>
2017-12-12 16:34:54 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Angry Professor/code.cs
+++ CORE CS/Algorithms/Implementation/Angry Professor/code.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+            int c = a.Where(x => x <= 0).Count();
+            if(c >= k) Console.WriteLine("NO");
+            else Console.WriteLine("YES");
+        }
+    }
+}
