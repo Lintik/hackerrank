d0bbdfe96eeadb5d74608c2e3a0f1f115e130a43
Marco <domandominguez@hotmail.com>
2017-12-13 19:23:21 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Viral Advertising/code.java
+++ CORE CS/Algorithms/Implementation/Viral Advertising/code.java
@@ -1,1 +1,16 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] m = new int[n+1];
+        m[0] = 2;
+        for(int i = 0; i < n - 1;i++)
+            m[i+1] = 3*m[i]/2;
+        System.out.println(Arrays.stream(m).sum());
+        in.close();
+    }
+}
