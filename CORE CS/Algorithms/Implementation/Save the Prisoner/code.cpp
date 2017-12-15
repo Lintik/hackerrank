4407abe946e55b70b8d8912f5744c5871d3a8e0d
Marco <domandominguez@hotmail.com>
2017-12-14 21:52:19 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Save the Prisoner/code.cpp
+++ CORE CS/Algorithms/Implementation/Save the Prisoner/code.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t;i++){
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        int s;
+        cin >> s;
+        cout << ((m + s - 2) % n) + 1 << endl; 
+    }
+    return 0;
+}
