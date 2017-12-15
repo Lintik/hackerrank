a1dda3a1f7443d215c5799c0382e09711e415be3
Marco <domandominguez@hotmail.com>
2017-12-14 22:07:27 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Save the Prisoner/code.cs
+++ CORE CS/Algorithms/Implementation/Save the Prisoner/code.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
+    static int saveThePrisoner(int n, int m, int s){
+        return ((s + m - 2) % n) + 1;
+    }
+
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int m = Convert.ToInt32(tokens_n[1]);
+            int s = Convert.ToInt32(tokens_n[2]);
+            int result = saveThePrisoner(n, m, s);
+            Console.WriteLine(result);
+        }
+    }
+}
