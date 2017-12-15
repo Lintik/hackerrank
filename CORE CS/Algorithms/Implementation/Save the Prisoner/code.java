47a981aed91cac5039769dc23dbe7af44619257e
Marco <domandominguez@hotmail.com>
2017-12-14 18:33:07 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Save the Prisoner/code.java
+++ CORE CS/Algorithms/Implementation/Save the Prisoner/code.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    static int saveThePrisoner(int n, int m, int s){
+        return ((m + s - 2) % n) + 1; 
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+    }
+}
