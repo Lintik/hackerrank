f11f9ffe68c8f169556911a4cb86f7c5e561ff24
Marco <domandominguez@hotmail.com>
2017-12-18 23:40:29 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Circular Array Rotation/code.cpp
+++ CORE CS/Algorithms/Implementation/Circular Array Rotation/code.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n, k, q;
+    cin >> n;
+    cin >> k;
+    cin >> q;
+    vector<int> a(n);
+    vector<int> b(n);
+    vector<int> m(q);
+    for(int i = 0;i < n; i++)
+        cin >> a[i];  
+    for(int i = 0;i < n;i++){
+        b[i] = a[(i + (n-(k%n)))%n];
+    }
+    for(int i = 0;i < q;i++){
+        int j;
+        cin >> j;
+        cout << b[j] << endl;
+    }
+    return 0;
+}
