aa5d4dac5956ab040b3d7cd99ec77be21f802aec
Marco <domandominguez@hotmail.com>
2017-12-19 04:56:11 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Circular Array Rotation/code.cs
+++ CORE CS/Algorithms/Implementation/Circular Array Rotation/code.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        int q = Convert.ToInt32(tokens_n[2]);
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+        for(int a0 = 0; a0 < q; a0++){
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a[(m + (n-(k%n)))%n]);
+        }
+    }
+}
