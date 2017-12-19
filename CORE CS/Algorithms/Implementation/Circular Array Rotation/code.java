a80df1a86832dc9d83fdcab3adf11581f32b1192
Marco <domandominguez@hotmail.com>
2017-12-19 04:55:11 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Circular Array Rotation/code.java
+++ CORE CS/Algorithms/Implementation/Circular Array Rotation/code.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int q = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        for(int a0 = 0; a0 < q; a0++){
+            int m = in.nextInt();
+            System.out.println(a[(m + (n-(k%n)))%n]);
+        }
+        
+    }
+}
