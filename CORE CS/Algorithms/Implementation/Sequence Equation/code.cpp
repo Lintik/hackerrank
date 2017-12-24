f25c9b13795a751fa0bc46c9a327aeb6babb3e70
Marco <domandominguez@hotmail.com>
2017-12-23 17:25:35 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Sequence Equation/code.cpp
+++ CORE CS/Algorithms/Implementation/Sequence Equation/code.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    vector<int> p_inverse(n+1);
+    for(int i = 1; i <= n;i++){
+        int px;
+        cin >> px; 
+        p_inverse[px] = i;
+    }
+    for (int x = 1; x <= n; x++) {
+        int y = p_inverse[p_inverse[x]];
+        cout << y << endl;
+    }
+    return 0;
+}
