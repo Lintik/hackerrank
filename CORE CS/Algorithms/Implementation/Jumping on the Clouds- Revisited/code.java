a3e730805d8c7cd956d7f0458027bff07b1947a8
Marco <domandominguez@hotmail.com>
2017-12-26 15:40:22 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Jumping on the Clouds- Revisited/code.java
+++ CORE CS/Algorithms/Implementation/Jumping on the Clouds- Revisited/code.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String args[] ) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int c[] = new int[n];
+        for(int c_i=0; c_i < n; c_i++){
+            c[c_i] = in.nextInt();
+        }
+        for(int i = 0, E = 100, p = 0; E >= 0;i++){
+            E--;
+            p += k % n;
+            p = p % n;
+            if(c[p] == 1) E -= 2;
+            if(p == 0){
+                System.out.println(E);
+                break;
+            }
+        }
+    }
+}
