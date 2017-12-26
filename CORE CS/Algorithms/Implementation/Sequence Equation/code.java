a125c73a32f493972d877f7c69d5c288973bcfc3
Marco <domandominguez@hotmail.com>
2017-12-26 07:34:29 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Sequence Equation/code.java
+++ CORE CS/Algorithms/Implementation/Sequence Equation/code.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] p_inverse = new int[n + 1];
+        for(int i = 1; i <= n;i++){
+            int px =  sc.nextInt();
+            p_inverse[px] = i;
+        }
+        for(int i = 1; i <= n;i++){
+            int y = p_inverse[p_inverse[i]];
+            System.out.println(y);
+        }
+    }
+}
