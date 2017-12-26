bfa9da921c28560b9bfdcd6f59d9d568268a29d3
Marco <domandominguez@hotmail.com>
2017-12-26 15:27:45 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Jumping on the Clouds- Revisited/code.cpp
+++ CORE CS/Algorithms/Implementation/Jumping on the Clouds- Revisited/code.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n, k, p, E;
+    cin >> n;
+    cin >> k;
+    vector<int> N(n);
+    for(int i = 0;i < n;i++)
+        cin >> N[i];
+    
+    for(int i = 0, E = 100, p = 0; E >= 0;i++){
+        E--;
+        p += k % n;
+        p = p % n;
+        if(N[p] == 1) E -= 2;
+        if(p == 0){
+            cout << E;
+            return 0;
+        }
+    }
+    return 0;
+}
