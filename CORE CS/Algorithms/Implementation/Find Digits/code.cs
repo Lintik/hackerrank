4c12f13609cd4f9849abaea395354e3602065653
Marco <domandominguez@hotmail.com>
2017-12-26 19:07:28 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Find Digits/code.cs
+++ CORE CS/Algorithms/Implementation/Find Digits/code.cs
@@ -1,1 +1,13 @@
+#!/bin/python3
 
+import sys
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    r = n
+    count = 0
+    while(r > 0):
+        if(r % 10 != 0 and n % (r % 10) == 0): count+=1
+        r = r // 10
+    print(count)
