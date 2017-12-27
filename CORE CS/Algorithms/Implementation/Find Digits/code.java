63104c83f154b5b50b253ec6d4619666c9fd08d0
Marco <domandominguez@hotmail.com>
2017-12-26 19:14:49 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Find Digits/code.java
+++ CORE CS/Algorithms/Implementation/Find Digits/code.java
@@ -1,1 +1,20 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t;i++){
+            int n = sc.nextInt();
+            int r = n;
+            int count = 0;
+            while(r > 0){
+                if(r % 10 != 0 && n % (r % 10) == 0) count++;
+                r /= 10;
+            }
+            System.out.println(count);
+        }
+    }
+}
