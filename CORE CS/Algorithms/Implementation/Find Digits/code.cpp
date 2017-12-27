6549d1c0f48456c5a323be7449f5a52985f81d53
Marco <domandominguez@hotmail.com>
2017-12-26 19:12:51 -0800

    Update code.cpp

--- CORE CS/Algorithms/Implementation/Find Digits/code.cpp
+++ CORE CS/Algorithms/Implementation/Find Digits/code.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        int n;
+        cin >> n;
+        int r = n;
+        int count = 0;
+        while(r > 0){
+            if(r % 10 != 0 && n % (r % 10) == 0) count++;
+            r /= 10;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
