412588982bd02b3288d5d0c3d50f802330220260
Marco <domandominguez@hotmail.com>
2018-01-10 23:28:03 -0800

    Update code.cs

--- CORE CS/Algorithms/Implementation/Extra long Factorials/code.cs
+++ CORE CS/Algorithms/Implementation/Extra long Factorials/code.cs
@@ -1,1 +1,18 @@
-
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        BigInteger Sum = new BigInteger();
+        int Factorial = Convert.ToInt32(Console.ReadLine());
+        Sum = 1;
+        for (int i = 1; i < Factorial + 1; i++){
+            Sum *= i;
+        }
+        Console.WriteLine(Sum);
+        Console.ReadLine();
+    }
+}
