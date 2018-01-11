16cd41be324f5fc7164dc0ed62ea7456fe89a4fa
Marco <domandominguez@hotmail.com>
2018-01-10 23:25:56 -0800

    Update code.java

--- CORE CS/Algorithms/Implementation/Extra long Factorials/code.java
+++ CORE CS/Algorithms/Implementation/Extra long Factorials/code.java
@@ -1,1 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+    
+    static BigInteger factorial(int n){
+        BigInteger product = BigInteger.ONE;
+        for(int i  = 2;i <= n;i++){
+            product = product.multiply(BigInteger.valueOf(i));
+        }
+        return product;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(factorial(sc.nextInt()));
+    }
+}
