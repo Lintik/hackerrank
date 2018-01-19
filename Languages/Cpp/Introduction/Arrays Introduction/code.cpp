7e7c97306d8e60587cc0fa79682aaa02579a6742
Marco <domandominguez@hotmail.com>
2018-01-19 03:11:50 -0800

    Update code.cpp

--- Languages/Cpp/Introduction/Arrays Introduction/code.cpp
+++ Languages/Cpp/Introduction/Arrays Introduction/code.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int i;
+    cin >> i;
+    int n[i];
+    for(int j = 0; j < i;j++){
+        cin >> n[j];
+    }
+    for(int j = i - 1;j >= 0;j--)
+        cout << n[j] << " ";
+    return 0;
+}
