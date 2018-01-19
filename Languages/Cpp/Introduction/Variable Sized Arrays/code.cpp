8fb4aec1d460141858e805ad3f08f8ca98f69d10
Marco <domandominguez@hotmail.com>
2018-01-19 11:07:06 -0800

    Update code.cpp

--- Languages/Cpp/Introduction/Variable Sized Arrays/code.cpp
+++ Languages/Cpp/Introduction/Variable Sized Arrays/code.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n, q;
+    cin >> n >> q;
+    int* a[n];
+    for(int i = 0; i < n; i++){
+        int m;
+        cin >> m;
+        a[i] = new int[m];
+        for(int j = 0;j < m;j++){
+            cin >> a[i][j]; 
+        }
+    }
+    for(int i = 0;i < q;i++){
+        int x, y;
+        cin >> x >> y;
+        cout << a[x][y] << endl;
+    }
+    return 0;
+}
