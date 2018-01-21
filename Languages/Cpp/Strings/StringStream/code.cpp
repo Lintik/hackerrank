50d942e269faae3d701893002a2c4cf44293b0a4
Marco <domandominguez@hotmail.com>
2018-01-21 15:07:53 -0800

    Update code.cpp

--- Languages/Cpp/Strings/StringStream/code.cpp
+++ Languages/Cpp/Strings/StringStream/code.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    char ch;
+    int a;
+    vector<int> v;
+    int n = count(str.begin(), str.end(), ',');
+    for(int i = 0;i < n;i++){
+        ss >> a >> ch;
+        v.push_back(a);
+    }
+    ss >> a;
+    v.push_back(a);
+    return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
