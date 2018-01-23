c4c7490b16cac72f8565ff6c7af9a5d78cf913df
Marco <domandominguez@hotmail.com>
2018-01-23 12:08:33 -0800

    Update code.js

--- CORE CS/Data Structure/Arrays/2D Array - DS/code.js
+++ CORE CS/Data Structure/Arrays/2D Array - DS/code.js
@@ -1,1 +1,43 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
 
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var arr = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
+    }
+    
+    var n = new Array(16);
+    var k = 0;
+    for(i = 0; i < 4;i++){
+        for(j = 0;j < 4;j++){
+            n[k] = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            k++;
+        }
+    }
+    var m = -1000;
+    for(a = 0; a < 16;a++)
+        m = m < n[a]? n[a]:m;
+    console.log(m);
+
+}
