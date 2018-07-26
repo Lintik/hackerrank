5db5a857ff1416f9c94016872e56660fac89c254
Marco <domandominguez@hotmail.com>
2018-07-26 11:29:57 -0700

    Update Hello_World_in_C.c

--- Languages/C/Hello_World_in_C.c
+++ Languages/C/Hello_World_in_C.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("Hello, World!\n");
+    printf("Welcome to C programming.");
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
