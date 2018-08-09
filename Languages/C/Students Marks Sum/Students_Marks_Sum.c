bae9d5896b523370232a95f55acdfbdff0cfbe49
Marco Domniguez <DomanDominguez@hotmail.com>
2018-08-09 10:49:30 -0700

    add solution

---
+++ Languages/C/Students Marks Sum/Students_Marks_Sum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+    int b = 0, g = 0;
+    for(int i = 0;i < number_of_students;i++){
+        if(i % 2 == 0)
+            b += *(marks + i);
+        else
+            g += *(marks + i);
+    }
+    return gender == 'b'?b:g;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
