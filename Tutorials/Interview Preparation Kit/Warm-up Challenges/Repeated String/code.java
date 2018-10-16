c38ce489460625abbf724a7c48819b46467d5b8c
Marco Domniguez <DomanDominguez@hotmail.com>
2018-10-16 15:40:44 -0700

    Update Tutorials/Interview Preparation Kit/Warm-up Challenges/Repeated String/code.java

---
+++ Tutorials/Interview Preparation Kit/Warm-up Challenges/Repeated String/code.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long c = 0;
+        for(int i = 0; i < s.length(); i++)
+            if(s.charAt(i) == 'a') c++;
+        long a = c * (n / s.length());
+        for(int i = 0; i < (n % s.length());i++)
+            if(s.charAt(i) == 'a') a++;
+        return a;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
