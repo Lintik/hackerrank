a070b548e7cf34d7be0f9f74a103b6dfdb9ed26c
Marco Domniguez <DomanDominguez@hotmail.com>
2018-10-15 17:04:26 -0700

    Update Tutorials/Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds/code.js

---
+++ Tutorials/Interview Preparation Kit/Warm-up Challenges/Jumping on the Clouds/code.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    let count = -1;
+    for (let i = 0; i < c.length; i++, count++)
+        if (i<c.length-2 && c[i+2]==0) i++;
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
