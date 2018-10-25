57f5e8d2bf929bd991e9cf4cb34993b04fa39968
Marco Domniguez <DomanDominguez@hotmail.com>
2018-10-24 22:17:07 -0700

    Update Tutorials/Interview Preparation Kit/Warm-up Challenges/Sock Merchant/code.js

---
+++ Tutorials/Interview Preparation Kit/Warm-up Challenges/Sock Merchant/code.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    ar.sort();
+    let t=0;
+    let c=1;
+    let prev = ar[0];
+    for(let i = 1;i<n;i++){
+        if(prev == ar[i])
+            c++;
+        else{
+            t+=Math.floor(c/2);
+            c=1;
+            prev=ar[i];
+        }
+    }
+    t+=Math.floor(c/2);
+    return t;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
