851fb17fb412a92639b9e60542bc1748b90a3d8a
Marco Domniguez <DomanDominguez@hotmail.com>
2018-11-03 19:53:35 -0700

    Update Tutorials/Interview Preparation Kit/Arrays/Arrays Left Rotation/code.js

---
+++ Tutorials/Interview Preparation Kit/Arrays/Arrays Left Rotation/code.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    const n = a.length;
+    let b = new Array(n);
+    d = d % n;
+    for(let i = d; i < n;i++)
+        b[i - d] = a[i];
+    for(let i = 0; i < d;i++)
+        b[i + n - d] = a[i];
+    return b;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
