52bc040b4e106e7e395bdc9c57543edff581c7cb
Marco Domniguez <DomanDominguez@hotmail.com>
2018-11-09 16:38:02 -0800

    Update Tutorials/10 Days of Javascript/Day 1 - Functions/code.js

---
+++ Tutorials/10 Days of Javascript/Day 1 - Functions/code.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function factorial(n){
+    let m = 1;
+    for(;n > 1;n--)
+        m *= n;
+    return m;
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
