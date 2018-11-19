e5dafb837bb100bfb00ea9a2dbc1c14ce98349e6
Marco Domniguez <DomanDominguez@hotmail.com>
2018-11-18 17:12:06 -0800

    Update code.js

---
+++ Tutorials/10 Days of Javascript/Day 2 - Conditional Statements Switch/code.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(s.charAt(0)){
+        case "a" || "e" || "i" || "o" || "u":
+            letter = "A";
+            break;
+        case "b" || "c" || "d" || "f" || "g":
+            letter = "B"
+            break;
+        case "h" || "j" || "k" || "l" || "m":
+            letter = "C";
+            break;
+        default:
+            letter = "D"
+            break;
+    }
+    return letter;
+}
+
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
