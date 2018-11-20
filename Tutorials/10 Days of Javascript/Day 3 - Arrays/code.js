1bfb2b344801097aa6fde447978a301699a2710a
Marco Domniguez <DomanDominguez@hotmail.com>
2018-11-20 13:40:48 -0800

    Update code.js

---
+++ Tutorials/10 Days of Javascript/Day 3 - Arrays/code.js
@@ -0,0 +1,65 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let first;
+    let second = 0;
+    if(nums[0] > nums[1]){
+        first = nums[0];
+        second = nums[1];
+    }
+    else if(nums[0] == nums[1])
+        first = nums[0];
+    else{
+        first = nums[1]
+        second = nums[0];
+    }
+    
+    for(let i = 1; i < nums.length;i++){
+        if(nums[i] > first){
+            second = first;
+            first = nums[i];
+        }
+        else if(nums[0] == first)
+            continue;
+        else if(nums[i] > second && nums[i] != first)
+            second = nums[i];
+        else
+            continue;
+    }
+    return second;
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
