04e23a89bb44de27d40d9da4cf075428b8147fb7
Lintik <domandominguez@hotmail.com>
2019-04-03 20:28:34 -0700

    Update code.cpp

--- Tutorials/Interview Preparation Kit/Arrays/Array Manipulation/code.cpp
+++ Tutorials/Interview Preparation Kit/Arrays/Array Manipulation/code.cpp
@@ -1,88 +1,27 @@
-#include <bits/stdc++.h>
-
+#include <cstdio>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-vector<string> split_string(string);
+int main() {
+    long int N, K, p, q, sum, i, j, max = 0, x = 0;
 
-// Complete the rotLeft function below.
-vector<int> rotLeft(vector<int> a, int d) {
-    vector<int>b;
-    d = d % a.size();
-    for(int i = d; i < a.size();i++)
-        b.push_back(a[i]);
-    for(int i = 0; i < d;i++)
-        b.push_back(a[i]);
-    return b;
-}
+    cin >> N >> K;
+    vector<long int> a(N + 1);
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nd_temp;
-    getline(cin, nd_temp);
-
-    vector<string> nd = split_string(nd_temp);
-
-    int n = stoi(nd[0]);
-
-    int d = stoi(nd[1]);
-
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
-
-    vector<string> a_temp = split_string(a_temp_temp);
-
-    vector<int> a(n);
-
-    for (int i = 0; i < n; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
+    for (i = 0; i < K; i++) {
+        cin >> p >> q >> sum;
+        a[p] += sum;
+        if ((q + 1) <= N)
+        a[q + 1] -= sum;
     }
 
-    vector<int> result = rotLeft(a, d);
-
-    for (int i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
+    for (i = 1; i <= N; i++) {
+        x = x + a[i];
+        if (max < x)
+        max = x;
     }
 
-    fout << "\n";
-
-    fout.close();
-
+    cout << max;
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
