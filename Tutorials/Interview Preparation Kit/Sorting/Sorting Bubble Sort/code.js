133c4d2648d102fb87c15263ab474204cb85f111
Lintik <domandominguez@hotmail.com>
2019-04-27 16:53:24 -0700

    Update code.js

---
+++ Tutorials/Interview Preparation Kit/Sorting/Sorting Bubble Sort/code.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countSwaps function below.
+function countSwaps(a) {
+    let count = 0;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < a.length - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                const t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+                count++;
+            }
+        }
+    }
+    
+    console.log("Array is sorted in %d swaps.", count);
+    console.log("First Element: %d", a[0]);
+    console.log("Last Element: %d", a[a.length - 1]);
+
+ 
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    countSwaps(a);
+}
