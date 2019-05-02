4ab9abf08c1ba445067842ebbe42b0b8c9f817cb
Lintik <domandominguez@hotmail.com>
2019-05-01 18:24:51 -0700

    Update code.cpp

--- Language Proficiency/Cpp/Classes/Virtual Functions/code.cpp
+++ Language Proficiency/Cpp/Classes/Virtual Functions/code.cpp
@@ -1,1 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
