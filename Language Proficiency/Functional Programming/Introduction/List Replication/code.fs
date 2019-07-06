713195f1636c6edcaad972c48490c74d9b534b7b
Marco <domandominguez@hotmail.com>
2019-07-05 20:37:55 -0700

    Update code.fs

--- Language Proficiency/Functional Programming/Introduction/List Replication/code.fs
+++ Language Proficiency/Functional Programming/Introduction/List Replication/code.fs
@@ -7,8 +7,5 @@ let main args =
             |> Seq.takeWhile (fun s -> s <> null)
             |> Seq.map int
             |> List.ofSeq
-    let s, xs = List.head inputs, List.tail inputs
-    List.iter (fun x ->
-        for i in 1..s do
-            printfn "%i" x) xs
+
     0
