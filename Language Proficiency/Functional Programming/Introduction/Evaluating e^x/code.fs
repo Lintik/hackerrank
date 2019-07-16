b0969d73127a1283c36bc74f9b64b41d3bc84c97
Lintik <domandominguez@hotmail.com>
2019-07-16 16:30:07 -0700

    update code.fs

--- Language Proficiency/Functional Programming/Introduction/Evaluating e^x/code.fs
+++ Language Proficiency/Functional Programming/Introduction/Evaluating e^x/code.fs
@@ -1,17 +1,17 @@
-    open System
-    let factorial v =
-            let rec fact c p =
-                    match c with
-                    | 0.0 -> p
-                    | _ -> fact <| c - 1.0 <| c * p
-            fact v 1.0
+open System
+let factorial v =
+        let rec fact c p =
+                match c with
+                | 0.0 -> p
+                | _ -> fact <| c - 1.0 <| c * p
+        fact v 1.0
 
-    let exp (ex:float List, x: float) =
-            List.map (fun xx -> 
-                            match xx with 
-                            | 0. -> 1.
-                            | xx -> x ** xx / factorial xx) ex
-    let n = Int32.Parse(Console.ReadLine())
-    for i in [1..n] do
-            let mutable x = Console.ReadLine() |> float
-            exp ([0.0..9.0], x) |> List.sum |> printfn "%f"
+let exp (ex:float List, x: float) =
+        List.map (fun xx -> 
+                        match xx with 
+                        | 0. -> 1.
+                        | xx -> x ** xx / factorial xx) ex
+let n = Int32.Parse(Console.ReadLine())
+for i in [1..n] do
+        let mutable x = Console.ReadLine() |> float
+        exp ([0.0..9.0], x) |> List.sum |> printfn "%f"
