381f33c0da5bf46be22b792c0800f83d4177190f
Marco <domandominguez@hotmail.com>
2019-07-19 17:44:21 -0700

    Update code.fs

---
+++ Language Proficiency/Functional Programming/Introduction/Functions or Not/code.fs
@@ -0,0 +1,41 @@
+open System
+
+let readList n parser =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.take n
+    |> Seq.map parser
+    |> Seq.toList
+
+let parseRelation (s: string) =
+    match s.Split(' ') with
+    | [|x|] -> int x, None
+    | [|k; v|] -> int k, Some (int v)
+    | _ -> failwith "OhGodWhy"
+
+let readTestCase () =
+    let n = int <| Console.ReadLine()
+
+    readList n parseRelation
+
+let isFunc relations =
+    relations
+    |> Map.ofList
+    |> Map.toSeq
+    |> Seq.length
+    |> (=) (List.length relations)
+
+let boolStr = function
+    | true -> "YES"
+    | false -> "NO"
+
+[<EntryPoint>]
+let main args =
+    let t = int <| Console.ReadLine()
+
+    Seq.initInfinite (fun _ -> readTestCase ())
+    |> Seq.take t
+    |> Seq.map isFunc
+    |> Seq.map boolStr
+    |> Seq.iter (printfn "%s")
+
+    0
