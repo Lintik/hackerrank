1e19fb38f33f332f593be7d191a6f4bb985272de
Marco <domandominguez@hotmail.com>
2019-07-21 17:22:10 -0700

    Update code.scala

--- Language Proficiency/Functional Programming/Introduction/Functions or Not/code.scala
+++ Language Proficiency/Functional Programming/Introduction/Functions or Not/code.scala
@@ -1,11 +1,11 @@
 object Solution {
-  def main(args: Array[String]) {
-    val n = io.StdIn.readInt()
-    (1 to n).foreach { i =>
-      val k = io.StdIn.readInt()
-      val list = (1 to k).map(x => io.StdIn.readLine.split("\\s+")(0)).toList
-      if (list.distinct.size == list.size) println("YES")
-      else println("NO")
+    def main(args: Array[String]) {
+        val n = io.StdIn.readInt()
+        (1 to n).foreach { i =>
+            val k = io.StdIn.readInt()
+            val list = (1 to k).map(x => io.StdIn.readLine.split("\\s+")(0)).toList
+            if (list.distinct.size == list.size) println("YES")
+            else println("NO")
+        }
     }
-  }
 }
