5726cdd76dc542fc5bda9bca40f5da712ad27848
Marco <domandominguez@hotmail.com>
2019-07-21 18:58:49 -0700

    Update code.fs

--- Language Proficiency/Functional Programming/Introduction/Compute the Perimeter of a Polygon/code.fs
+++ Language Proficiency/Functional Programming/Introduction/Compute the Perimeter of a Polygon/code.fs
@@ -1,17 +1,17 @@
 open System
 
-let getpoint()=
+let getPoint()=
     let a = Console.ReadLine().Split()
     (a.[0] |> int, a.[1] |> int)
     
-let calculate (a: int * int) (a2: int * int) =
-    sqrt (((pown (abs (fst a - fst a2)) 2) + (pown (abs (snd a - snd a2)) 2)) |> float)
+let getDistance (a: int * int) (b: int * int) =
+    sqrt (((pown (abs (fst a - fst b)) 2) + (pown (abs (snd a - snd b)) 2)) |> float)
 
 [<EntryPoint>]
 let main argv =
-    let n = Console.ReadLine() |> int
-    let points = [for i = 1 to n do yield getpoint()]
+    let n = int <| Console.ReadLine()
+    let points = [for i = 1 to n do yield getPoint()]
     let fpoints = points@[points.[0]]
-    let r = List.mapi(fun i a -> if i < fpoints.Length - 1 then calculate a fpoints.[i+1] else 0.0) fpoints
+    let r = List.mapi(fun i a -> if i < n then getDistance a fpoints.[i+1] else 0.0) fpoints
     Console.WriteLine(List.sum r)
     0
