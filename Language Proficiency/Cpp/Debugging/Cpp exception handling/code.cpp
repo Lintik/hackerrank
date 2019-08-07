30fd0f9e96bc01fce49b608ca9ee84b496a9e3a4
Marco <domandominguez@hotmail.com>
2019-08-06 17:54:52 -0700

    Update code.cpp

--- Language Proficiency/Cpp/Debugging/Cpp exception handling/code.cpp
+++ Language Proficiency/Cpp/Debugging/Cpp exception handling/code.cpp
@@ -19,8 +19,15 @@ int largest_proper_divisor(int n) {
 }
 
 void process_input(int n) {
-    int d = largest_proper_divisor(n);
-    cout << "result=" << d << endl;
+    try{
+        int d = largest_proper_divisor(n);
+        cout << "result=" << d << endl;
+    }
+    catch(invalid_argument &e){
+        cout << e.what() << endl;
+    }
+
+    cout << "returning control flow to caller" << endl;
 }
 
 
