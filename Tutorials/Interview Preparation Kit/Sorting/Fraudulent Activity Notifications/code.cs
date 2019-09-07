262a2fa6c48496728a1b6d66dadc2d67d69c7697
Marco <domandominguez@hotmail.com>
2019-09-06 18:07:04 -0700

    Update code.cs

--- Tutorials/Interview Preparation Kit/Sorting/Fraudulent Activity Notifications/code.cs
+++ Tutorials/Interview Preparation Kit/Sorting/Fraudulent Activity Notifications/code.cs
@@ -4,44 +4,27 @@ using System.IO;
 
 class Solution {
 
-    // Complete the activityNotifications function below.
-    static int activityNotifications(int[] expenditure, int d) {
+    static float Median(int[] count,int d){
         int d2 = (d+1)/2;
-            for (int i = 0; i < count.Length; i++)
-            {
-                if (count[i] > d2)
-                {
-                    return i;
-                }
-                if (count[i] == d2)
-                {
-                    if (d%2 != 0)
-                        return i;
-                    for (int j = i + 1; j < count.Length; j++)
-                    {
-                        if (count[j] > 0)
-                            return (i+j)/2.0f;
-                    }
-                }
-                d2 -= count[i];
+        for (int i = 0; i < count.Length; i++){
+            if (count[i] > d2) return i;
+            if (count[i] == d2){
+                if (d%2 != 0) return i;
+                for (int j = i + 1; j < count.Length; j++)
+                    if (count[j] > 0) return (i+j)/2.0f;
             }
-            return 0;
-
+            d2 -= count[i];
+        }
+        return 0;
     }
 
-    static void Main(string[] args) {
-        int[] nd = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-        int n = nd[0];
-        int d = nd[1];
+    static int activityNotifications(int[] a, int n,int d) {
         int result = 0;
         int[] count = new int[201];
 
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             int v = a[i];
-            if (i >= d)
-            {
+            if (i >= d){
                 var m = Median(count, d);
                 if (v >= 2*m)
                     result++;
@@ -49,7 +32,15 @@ class Solution {
             }
             count[v]++;
         }
+        return result;
+    }
 
-        Console.WriteLine(result);
+    static void Main(string[] args) {
+        int[] nd = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int n = nd[0];
+        int d = nd[1];
+
+        Console.WriteLine(activityNotifications(a,n,d));
     }
 }
