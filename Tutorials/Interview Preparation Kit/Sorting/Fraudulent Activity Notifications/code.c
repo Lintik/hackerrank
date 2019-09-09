6b427c81d86b61a926267bcfa79a4e520fd0e464
Marco <domandominguez@hotmail.com>
2019-09-08 18:37:32 -0700

    Update code.c

---
+++ Tutorials/Interview Preparation Kit/Sorting/Fraudulent Activity Notifications/code.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int median(int* count, int d){
+    int d2 = (d+1)/2;
+    for (int i = 0; i < 201; i++){
+        if (count[i] > d2) return 2*i;
+        if (count[i] == d2){
+            if (d%2 != 0) return 2*i;
+            for (int j = i + 1; j < 201; j++)
+                if (count[j] > 0) return i+j;
+        }
+        d2 -= count[i];
+    }
+    return 0;
+}
+
+int activityNotifications(int n, int* a, int d) {
+    int result = 0;
+        int count[201] = {0};
+
+        for (int i = 0; i < n; i++){
+            int v = a[i];
+            if (i >= d){
+                int m = median(count, d);
+                if (v >= m) result++;
+                count[a[i - d]]--;
+            }
+            count[v]++;
+        }
+        return result;
+}
+
+int main()
+{
+    int n, d;
+    scanf("%d", &n);
+    scanf("%d", &d);
+    int* a = (int *) malloc(n);
+    for(int i =0; i < n;i++)
+        scanf("%d", &a[i]);
+    int r = activityNotifications(n, a, d);
+    printf("%d", r);
+}
+    
