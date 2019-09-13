79463e7be89b9af98e5cef5a9f1ab83f961e4f58
Marco <domandominguez@hotmail.com>
2019-09-13 01:32:56 -0700

    Update code.cs

--- Tutorials/Interview Preparation Kit/Sorting/Merge Sort Counting Inversions/code.cs
+++ Tutorials/Interview Preparation Kit/Sorting/Merge Sort Counting Inversions/code.cs
@@ -5,17 +5,23 @@ using System;
 
 class Solution {
 
-    // Complete the countInversions function below.
-    static long countInversions(int[] arr) {
-        return mergeSort(arr)
+    static long count = 0;
+    static void countInversions(int[] arr) {
+        int[] a = mergeSort(arr);
     }
 
-    static long mergeSort(int[] arr){
-
+    static int[] mergeSort(int[] arr){
+        if(arr.Length == 1) return arr
+        int[] l1 = Array.Take(arr.Length/2 +1);
+        int[] l2 = Array.Skip(arr.Length/2 +1));
+        mergeSort(l1);
+        mergeSort(l2));
+        merge(l1,l2)
     }
 
-    static long merge(int[] arr, int count){
-        
+    static int[] merge(int[] l1, int[] l2, int count){
+        int[] c = new int[l1.Length +l2.Length ];
+        for()
     }
 
     static void Main(string[] args) {
