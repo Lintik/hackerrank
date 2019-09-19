c439f8056df2c298eed25214f7ca1196e9916fe8
Marco <domandominguez@hotmail.com>
2019-09-19 02:14:30 -0700

    Update code.java

--- Tutorials/Interview Preparation Kit/Sorting/Merge Sort Counting Inversions/code.java
+++ Tutorials/Interview Preparation Kit/Sorting/Merge Sort Counting Inversions/code.java
@@ -1,4 +1,14 @@
-public static long countInversions(int[] arr){
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static long countInversions(int[] arr){
         
         return mergeSort(arr, 0, arr.length - 1);
     }
@@ -41,3 +51,38 @@ public static long countInversions(int[] arr){
         System.arraycopy(newArr, 0, arr, start, end - start + 1); // Usual stuff from merge sort algorithm with arrays.
         return count;
     }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] arr = new int[n];
+
+            String[] arrItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int arrItem = Integer.parseInt(arrItems[i]);
+                arr[i] = arrItem;
+            }
+
+            long result = countInversions(arr);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
