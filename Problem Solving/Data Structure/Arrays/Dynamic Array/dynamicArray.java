c6d6c091e3db053e5ecc4df056a651cd1bead8d6
Marco <domandominguez@hotmail.com>
2019-11-18 14:33:34 -0800

    Update dynamicArray.java

---
+++ Problem Solving/Data Structure/Arrays/Dynamic Array/dynamicArray.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int Q = sc.nextInt();
+        List<List<Integer>> S = new List<List<Integer>>();
+        for(int i = 0;i < N;i++){
+            List<Integer> Stemp = new List<Integer>();
+            S.add(Stemp);
+        }
+        for(int i = 0;i < Q;i++){
+
+        }
+    }
+}
