a5b32de51bb2069e6bec5dc3104dce266d05f3b8
Marco <domandominguez@hotmail.com>
2020-01-24 10:33:34 -0800

    Create setsSTL.cpp

---
+++ Language Proficiency/Cpp/STL/Sets-STL/setsSTL.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void setSTL(set<int> &s, int y, int x){
+    
+}
+
+int main(){
+    int Q = 0;
+    cin >> Q;
+    set<int> s;
+    for(int i = 0;i < Q;i++){
+        int y = 0;
+        cin >> y;
+        int x = 0;
+        cin >> x;
+    }
+}
