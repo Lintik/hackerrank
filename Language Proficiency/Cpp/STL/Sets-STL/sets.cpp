08743e64a718f781c6e7ae4406502077340dafab
Marco <domandominguez@hotmail.com>
2020-01-24 10:31:06 -0800

    Update sets.cpp

--- Language Proficiency/Cpp/STL/Sets-STL/sets.cpp
+++ Language Proficiency/Cpp/STL/Sets-STL/sets.cpp
@@ -4,6 +4,18 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
+void setSTL(set<int> &s, int y, int x){
     
+}
+
+int main(){
+    int Q = 0;
+    cin >> Q;
+    set<int> s;
+    for(int i = 0;i < Q;i++){
+        int y = 0;
+        cin >> y;
+        int x = 0;
+        cin >> x;
+    }
 }
