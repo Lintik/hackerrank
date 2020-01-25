b39c83f8a686ebbfa75683f0a7380efdf60d73fe
Marco <domandominguez@hotmail.com>
2020-01-25 11:26:38 -0800

    Update printPretty.cpp

--- Language Proficiency/Cpp/STL/Print Pretty/printPretty.cpp
+++ Language Proficiency/Cpp/STL/Print Pretty/printPretty.cpp
@@ -11,7 +11,17 @@ int main() {
 		double B; cin >> B;
 		double C; cin >> C;
 
-		/* Enter your code here */
+		// LINE 1 
+        cout << hex << left << showbase << nouppercase; // formatting
+        cout << (long long) A << endl; // actual printed part
+
+        // LINE 2
+        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2); // formatting
+        cout << B << endl; // actual printed part
+
+        // LINE 3
+        cout << scientific << uppercase << noshowpos << setprecision(9); // formatting
+        cout << C << endl; // actual printed part
 
 	}
 	return 0;
