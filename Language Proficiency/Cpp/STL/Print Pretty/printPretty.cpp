6c9e1529fedc03144e1537f8a4e4c505b7893d08
Marco <domandominguez@hotmail.com>
2020-01-25 10:54:14 -0800

    Update printPretty.cpp

---
+++ Language Proficiency/Cpp/STL/Print Pretty/printPretty.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while(T--) {
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+
+		/* Enter your code here */
+
+	}
+	return 0;
+
+}
