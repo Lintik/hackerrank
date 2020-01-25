128325f91c5edd1c58b1452ec103e54850b5d85b
Marco <domandominguez@hotmail.com>
2020-01-25 13:18:29 -0800

    Update dequeSTL.cpp

---
+++ Language Proficiency/Cpp/STL/Deque-STL/dequeSTL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+
+void printKMax(int arr[], int n, int k){
+
+}
+
+int main(){
+    int t;
+	cin >> t;
+	while(t>0) {
+		int n,k;
+    	cin >> n >> k;
+    	int i;
+    	int arr[n];
+    	for(i=0;i<n;i++)
+      		cin >> arr[i];
+    	printKMax(arr, n, k);
+    	t--;
+  	}
+  	return 0;
+}
