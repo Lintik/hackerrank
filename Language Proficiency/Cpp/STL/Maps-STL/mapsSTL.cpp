f12fc701be58bc911c01b91395e4846c0146fc33
Marco <domandominguez@hotmail.com>
2020-01-24 18:11:14 -0800

    Update mapsSTL.cpp

---
+++ Language Proficiency/Cpp/STL/Maps-STL/mapsSTL.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+using namespace std;
+
+void mapDelete(map<string, int> &m){
+
+}
+
+void mapInsert(map<string, int> &m, int x){
+
+}
+
+int main(){
+    int Q = 0;
+    cin >> Q;
+    map<string,int> m;
+    for(int i = 0;i < Q;i++){
+        int x = 0, y = 0;
+        string name;
+        cin >> x >> name;
+        if(x == 1){
+            cin >> y;
+
+        }
+    }
+}
