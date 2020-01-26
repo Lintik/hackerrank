baec372fc4e4316d5850af06044239e032bb14f3
Marco <domandominguez@hotmail.com>
2020-01-25 18:54:02 -0800

    Update dequeSTL.cpp

--- Language Proficiency/Cpp/STL/Deque-STL/dequeSTL.cpp
+++ Language Proficiency/Cpp/STL/Deque-STL/dequeSTL.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <deque>
+#include <vector>
 using namespace std;
 
-void printKMax(int arr[], int n, int k){
+void printKMax(vector<int> &arr, int n, int k){
     deque<int> dq;
+    int m = 0;
     for(int i = 0;i < k;i++){
-        
+        dq.push_back(arr[i]);
+        if(arr[i] > m)
+            m = arr[i];
     }
+    cout << m;
+    for(int i = k;i < n;i++){
+        if(arr[i-k] == m){
+            m = 0;
+            dq.pop_front();
+            dq.push_back(arr[i]);
+            for(int j = 0;j < k;j++){
+                if(dq[j] > m)
+                    m = dq[j];
+            }
+        }
+        if(arr[i] >= m){
+            m = arr[i];
+            dq.pop_front();
+            dq.push_back(arr[i]);
+        }
+        else{
+            dq.pop_front();
+            dq.push_back(arr[i]);
+        }
+        cout << " " << m;
+    }
+    cout << endl;
 }
 
 int main(){
@@ -16,7 +43,7 @@ int main(){
 		int n,k;
     	cin >> n >> k;
     	int i;
-    	int arr[n];
+    	vector<int> arr(n);
     	for(i=0;i<n;i++)
       		cin >> arr[i];
     	printKMax(arr, n, k);
