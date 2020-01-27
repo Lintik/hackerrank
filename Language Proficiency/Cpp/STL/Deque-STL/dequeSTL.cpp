43d442e8b0c3f0b1f964e101b8d79f7917c66091
Marco <domandominguez@hotmail.com>
2020-01-26 16:13:59 -0800

    Update dequeSTL.cpp

--- Language Proficiency/Cpp/STL/Deque-STL/dequeSTL.cpp
+++ Language Proficiency/Cpp/STL/Deque-STL/dequeSTL.cpp
@@ -1,52 +1,56 @@
 #include <iostream>
-#include <deque>
-#include <vector>
+#include <deque> 
 using namespace std;
 
-void printKMax(vector<int> &arr, int n, int k){
-    deque<int> dq;
-    int m = 0;
-    for(int i = 0;i < k;i++){
-        dq.push_back(arr[i]);
-        if(arr[i] > m)
-            m = arr[i];
-    }
-    cout << m;
-    for(int i = 1;i < n - k + 1;i++){
-        if(dq[0] == m){
-            m = arr[i+k-1];
+void printKMax(int arr[], int n, int k) 
+{ 
+    // Double ended Queue that stores indices
+    deque<int> dq(k); 
+  
+    // populate dq with the first iteration
+    int i; 
+    for (i = 0; i < k; ++i) { 
+        // remove insignificant indices
+        while ((!dq.empty()) && arr[i] >= arr[dq.back()]) 
+            dq.pop_back(); // Remove from rear 
+        dq.push_back(i); // Add new element at rear of queue 
+    } 
+  
+    // Process rest of the elements
+    for (; i < n; ++i) { 
+        cout << arr[dq.front()] << " "; 
+  
+        // Remove out-of-bounds indices
+        while ((!dq.empty()) && dq.front() <= i - k) 
             dq.pop_front();
-            dq.push_back(arr[i+k-1]);
-            for(int j = 0;j < k;j++){
-                if(dq[j] > m)
-                    m = dq[j];
-            }
-        }
-        if(arr[i+k-1] > m){
-            m = arr[i+k-1];
-            dq.pop_front();
-            dq.push_back(arr[i+k-1]);
-        }
-        else{
-            dq.pop_front();
-            dq.push_back(arr[i+k-1]);
-        }
-        cout << " " << m;
-    }
+  
+        // Remove elements smaller than the current 
+        while ((!dq.empty()) && arr[i] >= arr[dq.back()]) 
+            dq.pop_back(); 
+  
+        // Add current element at the rear of Qi 
+        dq.push_back(i); 
+    } 
+  
+    cout << arr[dq.front()];
     cout << endl;
-}
+} 
+  
 
 int main(){
-    int t;
+  
+	int t;
 	cin >> t;
 	while(t>0) {
 		int n,k;
     	cin >> n >> k;
-    	vector<int> arr(n);
-    	for(int i = 0;i < n;i++)
+    	int i;
+    	int arr[n];
+    	for(i=0;i<n;i++)
       		cin >> arr[i];
     	printKMax(arr, n, k);
     	t--;
   	}
   	return 0;
 }
+
