0a0e95a7e4f43a5cb1b4bc9be4487898c98788e1
Marco <domandominguez@hotmail.com>
2020-01-27 17:46:41 -0800

    Update magicSpells.cpp

--- Language Proficiency/Cpp/Inheritance/Magic Spells/magicSpells.cpp
+++ Language Proficiency/Cpp/Inheritance/Magic Spells/magicSpells.cpp
@@ -61,9 +61,34 @@ class SpellJournal {
 string SpellJournal::journal = "";
 
 void counterspell(Spell *spell) {
-
-/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-
+    if(typeid(*spell) == typeid(Fireball)) {
+        ((Fireball*)spell)->revealFirepower();
+    }
+    else if(typeid(*spell) == typeid(Frostbite)) {
+        ((Frostbite*)spell)->revealFrostpower();
+    }
+    else if(typeid(*spell) == typeid(Thunderstorm)) {
+        ((Thunderstorm*)spell)->revealThunderpower();
+    }
+    else if(typeid(*spell) == typeid(Waterbolt)) {
+        ((Waterbolt*)spell)->revealWaterpower();
+    }
+    else {
+        string spellName = spell->revealScrollName();
+        string spellJournal = SpellJournal::journal;
+        // LCS Algorithm
+        string &a = spellName, &b = spellJournal;
+        vector<vector<int> > dp(a.size() + 1, vector<int>(b.size() + 1));
+        for(int i = 1; i <= a.size(); i++) {
+            for(int j = 1; j <= b.size(); j++) {
+                if(a[i - 1] == b[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        cout << dp[a.size()][b.size()] << endl;
+    }
 }
 
 class Wizard {
