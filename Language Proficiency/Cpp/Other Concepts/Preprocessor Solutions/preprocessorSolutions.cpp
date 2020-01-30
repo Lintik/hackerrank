772b26e4dd911b5aeac6be725a8b4df63a1b87ce
Marco <domandominguez@hotmail.com>
2020-01-29 17:26:04 -0800

    Update preprocessorSolutions.cpp

---
+++ Language Proficiency/Cpp/Other Concepts/Preprocessor Solutions/preprocessorSolutions.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
