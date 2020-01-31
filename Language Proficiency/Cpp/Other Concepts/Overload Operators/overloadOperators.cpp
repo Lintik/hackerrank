cc1f8c6b0fba982cd9aac2f47f96908185f90e74
Marco <domandominguez@hotmail.com>
2020-01-31 12:40:58 -0800

    Update overloadOperators.cpp

--- Language Proficiency/Cpp/Other Concepts/Overload Operators/overloadOperators.cpp
+++ Language Proficiency/Cpp/Other Concepts/Overload Operators/overloadOperators.cpp
@@ -3,28 +3,24 @@ using namespace std;
 
 class Complex{
     public:
-        int a,b;
-        void input(string s)
-        {
-            int v1=0;
-            int i=0;
-            while(s[i]!='+')
-            {
-                v1=v1*10+s[i]-'0';
+        int a, b;
+        void input(string s){
+            int v1 = 0;
+            int i = 0;
+            while(s[i] != '+'){
+                v1 = v1 * 10 + s[i] - '0';
                 i++;
             }
             while(s[i]==' ' || s[i]=='+'||s[i]=='i')
-            {
                 i++;
-            }
+
             int v2=0;
-            while(i<s.length())
-            {
+            while(i<s.length()){
                 v2=v2*10+s[i]-'0';
                 i++;
             }
-            a=v1;
-            b=v2;
+            a = v1;
+            b = v2;
         }
 };
 
