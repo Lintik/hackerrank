3968658b544513745f28a8342cecce5576e00b5c
Marco <domandominguez@hotmail.com>
2020-01-31 12:37:13 -0800

    Update overloadOperators.cpp

--- Language Proficiency/Cpp/Other Concepts/Overload Operators/overloadOperators.cpp
+++ Language Proficiency/Cpp/Other Concepts/Overload Operators/overloadOperators.cpp
@@ -1,35 +1,31 @@
-//Operator Overloading
-
 #include<iostream>
-
 using namespace std;
 
-class Complex
-{
-public:
-    int a,b;
-    void input(string s)
-    {
-        int v1=0;
-        int i=0;
-        while(s[i]!='+')
+class Complex{
+    public:
+        int a,b;
+        void input(string s)
         {
-            v1=v1*10+s[i]-'0';
-            i++;
+            int v1=0;
+            int i=0;
+            while(s[i]!='+')
+            {
+                v1=v1*10+s[i]-'0';
+                i++;
+            }
+            while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+            {
+                i++;
+            }
+            int v2=0;
+            while(i<s.length())
+            {
+                v2=v2*10+s[i]-'0';
+                i++;
+            }
+            a=v1;
+            b=v2;
         }
-        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
-        {
-            i++;
-        }
-        int v2=0;
-        while(i<s.length())
-        {
-            v2=v2*10+s[i]-'0';
-            i++;
-        }
-        a=v1;
-        b=v2;
-    }
 };
 
 ostream& operator<<(ostream& os, const Complex& c) {
