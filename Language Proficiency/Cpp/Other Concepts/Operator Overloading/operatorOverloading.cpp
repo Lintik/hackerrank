6fa1dafdac48bd1b5b42ce168c414bd06a624625
Marco <domandominguez@hotmail.com>
2020-01-30 21:43:55 -0800

    Update operatorOverloading.cpp

--- Language Proficiency/Cpp/Other Concepts/Operator Overloading/operatorOverloading.cpp
+++ Language Proficiency/Cpp/Other Concepts/Operator Overloading/operatorOverloading.cpp
@@ -1,9 +1,21 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
+
+class Matrix{
+
+    public:
+        vector<vector<int> > a;
+
+        Matrix & operator + (const Matrix &y) {
+            for (int m=0; m<y.a.size(); ++m) 
+                for (int n=0; n<y.a[0].size(); ++n) 
+                    this->a[m][n] = this->a[m][n] + y.a[m][n];
+        
+            return *this;
+        }
+
+};
 
 int main () {
    int cases,k;
