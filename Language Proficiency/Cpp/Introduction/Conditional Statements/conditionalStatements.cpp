1c01c70e9ab0fd68ab04908b315b62b9fa57858c
Marco <domandominguez@hotmail.com>
2020-01-30 18:18:15 -0800

    Create conditionalStatements.cpp

---
+++ Language Proficiency/Cpp/Introduction/Conditional Statements/conditionalStatements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+    if(s == 1) cout << "one";
+    else if(s == 2) cout << "two";
+    else if(s == 3) cout << "three";
+    else if(s == 4) cout << "four";
+    else if(s == 5) cout << "five";
+    else if(s == 6) cout << "six";
+    else if(s == 7) cout << "seven";
+    else if(s == 8) cout << "eight";
+    else if(s == 9) cout << "nine";
+    else cout << "Greater than 9";
+    return 0;
+}
