f1a18a87380ce9f31c8c66aaeb7cf0d1a1ccbaa0
Marco <domandominguez@hotmail.com>
2020-02-04 17:54:09 -0800

    Update messagesOrder.cpp

---
+++ Language Proficiency/Cpp/Debugging/Messages Order/messagesOrder.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Message {
+public: 
+    Message() {}
+    const string& get_text() {
+        
+    }
+};
+
+class MessageFactory {
+public:
+    MessageFactory() {}
+    Message create_message(const string& text) {
+        
+    }
+};
+
+class Recipient {
+public:
+    Recipient() {}
+    void receive(const Message& msg) {
+        messages_.push_back(msg);
+    }
+    void print_messages() {
+        fix_order();
+        for (auto& msg : messages_) {
+            cout << msg.get_text() << endl;
+        }
+        messages_.clear();
+    }
+private:
+    void fix_order() {
+        sort(messages_.begin(), messages_.end());
+    }
+    vector<Message> messages_;
+};
+
+class Network {
+public:
+    static void send_messages(vector<Message> messages, Recipient& recipient) {
+    // simulates the unpredictable network, where sent messages might arrive in unspecified order
+        random_shuffle(messages.begin(), messages.end());         
+        for (auto msg : messages) {
+            recipient.receive(msg);
+        }
+    }
+};
+
+
+
+int main() {
+    MessageFactory message_factory;
+    Recipient recipient;
+    vector<Message> messages;
+    string text;
+    while (getline(cin, text)) {
+        messages.push_back(message_factory.create_message(text));
+    }
+    Network::send_messages(messages, recipient);
+    recipient.print_messages();
+}
