d37762f2fccc5ea7a5e5b05a5f9b77f3332bc197
Marco <domandominguez@hotmail.com>
2020-02-05 18:25:27 -0800

    Update attendingWorkshops.cpp

--- Language Proficiency/Cpp/Other Concepts/Attending Workshops/attendingWorkshops.cpp
+++ Language Proficiency/Cpp/Other Concepts/Attending Workshops/attendingWorkshops.cpp
@@ -2,8 +2,51 @@
 
 using namespace std;
 
-//Define the structs Workshops and Available_Workshops.
-//Implement the functions initialize and CalculateMaxWorkshops
+struct Workshops {
+    int start_time, duration, end_time; 
+};
+
+struct Available_Workshops {
+    int n;
+    vector <Workshops> w;
+};
+
+Available_Workshops *initialize(int *start_time, int *duration, int n) {
+    Available_Workshops *paw=new(Available_Workshops);
+    Workshops ws;
+    
+    paw->n=n;
+    for (int i=0;i<n;i++) {
+        //construct a Workshops
+        ws.start_time=start_time[i];
+        ws.duration=duration[i];
+        ws.end_time=start_time[i]+duration[i];
+        paw->w.push_back(ws);   
+    }
+        
+    return paw;    
+}
+
+bool compare(Workshops w1, Workshops w2) { 
+    return (w1.end_time < w2.end_time); 
+}
+
+int CalculateMaxWorkshops(Available_Workshops* ptr) {
+    // sort workshops by end_time
+    sort(ptr->w.begin(),ptr->w.end(),compare); 
+    
+    // interval scheduling
+    int last_processed_time=-1;
+    int maxWorkshops=0;
+    for (int i=0;i<ptr->n;i++) {
+        if (ptr->w[i].start_time >= last_processed_time) {
+            last_processed_time=ptr->w[i].end_time;
+            maxWorkshops++;
+        }
+    }
+
+    return maxWorkshops;
+}
 
 int main(int argc, char *argv[]) {
     int n; // number of workshops
