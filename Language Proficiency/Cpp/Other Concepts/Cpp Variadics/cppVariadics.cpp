516d9ea5341e3e2cdcaee52d09a0243147cf0f47
Marco <domandominguez@hotmail.com>
2020-02-06 20:28:42 -0800

    Update cppVariadics.cpp

--- Language Proficiency/Cpp/Other Concepts/Cpp Variadics/cppVariadics.cpp
+++ Language Proficiency/Cpp/Other Concepts/Cpp Variadics/cppVariadics.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// Enter your code for reversed_binary_value<bool...>()
+template <bool a> int reversed_binary_value() { return a; }
+
+template <bool a, bool b, bool... d> int reversed_binary_value() {
+  return (reversed_binary_value<b, d...>() << 1) + a;
+}
 
 
 template <int n, bool...digits>
