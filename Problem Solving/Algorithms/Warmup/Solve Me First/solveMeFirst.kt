842fadc525f3e508f4ac691d05b65c598039b2fa
Marco <domandominguez@hotmail.com>
2020-02-07 20:27:04 -0800

    Create solveMeFirst.kt

---
+++ Problem Solving/Algorithms/Warmup/Solve Me First/solveMeFirst.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun solveMeFirst(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
+
