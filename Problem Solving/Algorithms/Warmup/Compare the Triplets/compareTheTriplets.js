a7ad7a980f5d41c5673018ea0a9eea5d813076ca
Marco <domandominguez@hotmail.com>
2020-02-08 20:01:48 -0800

    Update compareTheTriplets.js

---
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTriplets.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let r = [0,0];
+    for(let i = 0;i < 3;i++){
+        if(a[i] > b[i])
+            r[0]++;
+        else if(a[i] < b[i])
+            r[1]++;
+        else   
+            continue;
+    }
+    return r;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
