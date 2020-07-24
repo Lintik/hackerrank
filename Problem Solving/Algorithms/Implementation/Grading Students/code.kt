71bcb866235d41100efc7063c1a6fbea25e6c569
Lintik <domandominguez@hotmail.com>
2020-07-23 17:42:24 -0700

    update code.kt

---
+++ Problem Solving/Algorithms/Implementation/Grading Students/code.kt
@@ -0,0 +1,44 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+    // Write your code here
+
+}
+
+fun main(args: Array<String>) {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array<Int>(gradesCount, { 0 })
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
+}
