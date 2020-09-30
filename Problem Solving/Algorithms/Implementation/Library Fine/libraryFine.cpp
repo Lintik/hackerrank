1b3c89db7465104dbfee93092358f0f6c062063f
Lintik <domandominguez@hotmail.com>
2020-09-29 23:06:58 -0700

    update libraryFine.cpp

--- Problem Solving/Algorithms/Implementation/Library Fine/libraryFine.cpp
+++ Problem Solving/Algorithms/Implementation/Library Fine/libraryFine.cpp
@@ -1,46 +1,21 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<string> split_string(string);
+int monthToDays(m1){
+    int[] m = [0, 31,59,90,120,151,181,212,243,273,304,334];
+    return m[m1];
+}
 
 // Complete the libraryFine function below.
 int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
-
-
+    int diff = 0;
+    // convert the dates to days and find the difference
+    int y = y1 - y2;
+    int d = monthToDays(m1) + d1 - monthToDays(m2) - d2;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string d1M1Y1_temp;
-    getline(cin, d1M1Y1_temp);
-
-    vector<string> d1M1Y1 = split_string(d1M1Y1_temp);
-
-    int d1 = stoi(d1M1Y1[0]);
-
-    int m1 = stoi(d1M1Y1[1]);
-
-    int y1 = stoi(d1M1Y1[2]);
-
-    string d2M2Y2_temp;
-    getline(cin, d2M2Y2_temp);
-
-    vector<string> d2M2Y2 = split_string(d2M2Y2_temp);
-
-    int d2 = stoi(d2M2Y2[0]);
-
-    int m2 = stoi(d2M2Y2[1]);
-
-    int y2 = stoi(d2M2Y2[2]);
-
-    int result = libraryFine(d1, m1, y1, d2, m2, y2);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
+int main(){
+    int d1, d2, m1, m2, y1, y2;
+    cin >> d1 >> m1 >> y1 >> d2 >> m2 >> y2; 
+    cout << libraryFine(d1, m1, y1, d2, m2, y2);
 }
-
