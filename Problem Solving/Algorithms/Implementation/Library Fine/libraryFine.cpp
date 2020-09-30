0580004564a34e0a6d4e97e1c4ffb5b58afb6fb2
Lintik <domandominguez@hotmail.com>
2020-09-30 00:23:41 -0700

    update libraryFine.cpp

--- Problem Solving/Algorithms/Implementation/Library Fine/libraryFine.cpp
+++ Problem Solving/Algorithms/Implementation/Library Fine/libraryFine.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int monthToDays(m1){
-    int[] m = [0, 31,59,90,120,151,181,212,243,273,304,334];
-    return m[m1];
-}
-
-// Complete the libraryFine function below.
 int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
-    int diff = 0;
-    // convert the dates to days and find the difference
-    int y = y1 - y2;
-    int d = monthToDays(m1) + d1 - monthToDays(m2) - d2;
+    if(y1 < y2 || (y1 == y2 && m1 <= m2 && d1 <= d2)){
+        return 0;
+    }
+    else if (y1 == y2 && m1 == m2 && d1 > d2){
+        return 15 * (d1 - d2);
+    }
+    else if (y1 == y2 && m1 > m2){
+        return 500 * (m1 - m2);
+    }
+    else{
+        return 10000;
+    }
 }
 
 int main(){
