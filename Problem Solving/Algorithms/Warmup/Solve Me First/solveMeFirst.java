08d7d54308aecbe6ab243b3537e551c6592d7fa6
Lintik <domandominguez@hotmail.com>
2020-10-07 18:28:06 -0700

    fix spacing

--- Problem Solving/Algorithms/Warmup/Solve Me First/solveMeFirst.java
+++ Problem Solving/Algorithms/Warmup/Solve Me First/solveMeFirst.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
 
-    static int solveMeFirst(int a, int b) {
-         // Hint: Type return a+b; below
-        return a + b;
-   }
+     static int solveMeFirst(int a, int b) {
+          return a + b;
+     }
    
- public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a;
-        a = in.nextInt();
-        int b;
-        b = in.nextInt();
-        int sum;
-        sum = solveMeFirst(a, b);
-        System.out.println(sum);
-        in.close();
+     public static void main(String[] args) {
+          Scanner in = new Scanner(System.in);
+          int a;
+          a = in.nextInt();
+          int b;
+          b = in.nextInt();
+          int sum;
+          sum = solveMeFirst(a, b);
+          System.out.println(sum);
+          in.close();
    }
 }
