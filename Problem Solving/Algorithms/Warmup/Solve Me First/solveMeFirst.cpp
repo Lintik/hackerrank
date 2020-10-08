72351e93f0c801988b44ba011fd8d8d4098d508e
Lintik <domandominguez@hotmail.com>
2020-10-07 18:25:07 -0700

    remove unecessary lines

--- Problem Solving/Algorithms/Warmup/Solve Me First/solveMeFirst.cpp
+++ Problem Solving/Algorithms/Warmup/Solve Me First/solveMeFirst.cpp
@@ -1,13 +1,7 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-
 int solveMeFirst(int a, int b) {
- // Hint: Type return a+b; below:
     return a + b;
   
 }
