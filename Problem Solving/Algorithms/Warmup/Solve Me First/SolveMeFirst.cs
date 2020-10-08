61ec4e8358a851b9e4b194b8657ff2bf8489a999
Lintik <domandominguez@hotmail.com>
2020-10-07 17:58:27 -0700

    rename solveMeFirst.cs

--- Problem Solving/Algorithms/Warmup/Solve Me First/SolveMeFirst.cs
+++ Problem Solving/Algorithms/Warmup/Solve Me First/SolveMeFirst.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 class Solution {
     static int solveMeFirst(int a, int b) { 
-      // Hint: Type return a+b; below
       return a + b;
     }
+    
     static void Main(String[] args) {
         int val1 = Convert.ToInt32(Console.ReadLine());
         int val2 = Convert.ToInt32(Console.ReadLine());
