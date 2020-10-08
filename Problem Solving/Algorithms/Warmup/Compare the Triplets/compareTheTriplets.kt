dca9595c408ca1d22b44bdc9b506065d98c51bd6
Lintik <domandominguez@hotmail.com>
2020-10-07 20:51:59 -0700

    update compareTheTriplet.kt

--- Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTriplets.kt
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTriplets.kt
@@ -2,8 +2,18 @@ import java.util.*
 
 // Complete the compareTriplets function below.
 fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    val r = arrayOf<Int>(0,0)
+    
+    if(a[0] > b[0]) r[0] += 1
+    else if(a[0] < b[0])  r[1] += 1
 
+    if(a[1] > b[1])  r[0] += 1
+    else if(a[1] < b[1])  r[1] += 1
 
+    if(a[2] > b[2])  r[0] += 1
+    else if(a[2] < b[2])  r[1] += 1
+
+    return r
 }
 
 fun main(args: Array<String>) {
