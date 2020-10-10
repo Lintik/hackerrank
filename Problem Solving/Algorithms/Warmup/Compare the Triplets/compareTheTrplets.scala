81e196b8d029c60129556c57b701d48237d34336
Lintik <domandominguez@hotmail.com>
2020-10-09 19:32:57 -0700

    update compareTheTriplets.scala

--- Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
@@ -4,18 +4,7 @@ object Solution {
 
     // Complete the compareTriplets function below.
     def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
-        int[] r = new int[]{0,0}
-
-        if(a0 > b0) r[0]++
-        else if(a0 < b0) r[1]++
-
-        if(a1 > b1) r[0]++
-        else if(a1 < b1) r[1]++
-
-        if(a2 > b2) r[0]++
-        else if(a2 < b2) r[1]++
         
-        return r
     }
 
     def main(args: Array[String]) {
