2cc815c6b25fd10a1012e72a62ccea88f206c7f4
Lintik <domandominguez@hotmail.com>
2020-10-09 20:41:12 -0700

    update compareTheTriplets.scala

--- Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
@@ -2,23 +2,18 @@ import java.text._
 
 object Solution {
 
-    // Complete the compareTriplets function below.
-    def compareTriplets(alice: Array[Int], bob: Array[Int]) {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val alice = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val bob = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
         val (alicePoints, bobPoints) = alice.zip(bob).map { case (a, b) =>
             if (a > b) (1, 0)
             else if (b > a) (0, 1)
             else (0, 0)
             }.unzip
-        println(s"${alicePoints.sum} ${bobPoints.sum}")      
-        
-    }
-
-    def main(args: Array[String]) {
-        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-
-        val a = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
-
-        val b = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
-        val result = compareTriplets(a, b)
+        println(s"${alicePoints.sum} ${bobPoints.sum}")
     }
 }
