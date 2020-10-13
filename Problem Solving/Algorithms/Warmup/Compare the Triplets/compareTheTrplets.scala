c6f068d34fe5233cbef7150d594cfe20b54c2e86
Lintik <domandominguez@hotmail.com>
2020-10-12 17:42:30 -0700

    update compareTheTriplets.scala

--- Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
+++ Problem Solving/Algorithms/Warmup/Compare the Triplets/compareTheTrplets.scala
@@ -1,22 +1,19 @@
+import java.io._
 import java.text._
 
-object Solution {
+object Solution extends App {
+    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
 
-    // Complete the compareTriplets function below.
-    def compareTriplets(a: Array[Int], b: Array[Int]): Array[Int] = {
-        
+    val lines = io.Source.stdin.getLines()
+    val total = lines.next().split(" ").map(_.toInt).zip(lines.next().split(" ").map(_.toInt)).fold((0,0)){ case (acc, pair) =>
+    if(pair._1 > pair._2)
+        (acc._1+1, acc._2)
+    else if(pair._1 < pair._2)
+        (acc._1, acc._2+1)
+    else
+        acc
     }
-
-    def main(args: Array[String]) {
-        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-
-        val a = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
-
-        val b = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
-        val result = compareTriplets(a, b)
-
-        printWriter.println(result.mkString(" "))
-
-        printWriter.close()
-    }
+    
+    printWriter.println(s"${total._1} ${total._2}")
+    printWriter.close()
 }
