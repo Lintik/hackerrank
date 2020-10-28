4e1fe7e46499a7907398bfe501538e57a7c9fa66
Marco D <domandominguez@hotmail.com>
2020-10-27 23:16:31 -0700

    Update diagonalDifference.cpp

--- Problem Solving/Algorithms/Warmup/Diagonal Difference/diagonalDifference.cpp
+++ Problem Solving/Algorithms/Warmup/Diagonal Difference/diagonalDifference.cpp
@@ -1,23 +1,23 @@
-#include <vector>
 #include <iostream>
 using namespace std;
 
-
-int main(){
+int main() {
     int n;
     cin >> n;
-    vector< vector<int> > a(n,vector<int>(n));
-    for(int a_i = 0;a_i < n;a_i++){
-       for(int a_j = 0;a_j < n;a_j++){
-          cin >> a[a_i][a_j];
-       }
+
+    int arr[n][n];
+
+    int d1=0;
+    int d2=0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+            if (i == j) d1 += arr[i][j];
+            if (i == n - j - 1) d2 += arr[i][j];
+        }
     }
-    int d1 = 0;
-    int d2 = 0;
-    for(int i=0;i<n;i++)
-        d1+=a[i][i];
-    for(int i=0, j = n-1; i<n;i++,j--)
-        d2+=a[i][j];
-    cout << abs(d1-d2);
+
+    cout << abs(d1 - d2) << endl;
     return 0;
 }
