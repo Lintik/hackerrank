be28cc80a83bc82e1f3436142f98e141b8032100
Marco D <domandominguez@hotmail.com>
2020-10-28 23:37:16 -0700

    update diagonalDifference.kt

--- Core CS/Algorithms/Warmup/Diagonal Difference/diagonalDifference.kt
+++ Core CS/Algorithms/Warmup/Diagonal Difference/diagonalDifference.kt
@@ -1,8 +1,18 @@
 import java.io.*
 import java.util.*
+import kotlin.math.absoluteValue
 
 fun diagonalDifference(arr: Array<Array<Int>>): Int {
-
+    var d1 = 0
+    var d2 = 0
+    val n = arr.size - 1
+    for (i in 0..n) {
+        for (j in 0..n) {
+            if (i == j) d1 += arr[i][j]
+            if (i == arr.size - j - 1) d2 += arr[i][j]
+        }
+    }
+    return (d1 - d2).absoluteValue
 }
 
 fun main(args: Array<String>) {
