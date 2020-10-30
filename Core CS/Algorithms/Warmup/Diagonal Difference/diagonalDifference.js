6edce9d74e77d86265dfa1f331b88fa40a548320
Marco D <domandominguez@hotmail.com>
2020-10-29 19:55:44 -0700

    update diagonalDifference.js

--- Core CS/Algorithms/Warmup/Diagonal Difference/diagonalDifference.js
+++ Core CS/Algorithms/Warmup/Diagonal Difference/diagonalDifference.js
@@ -1,19 +1,20 @@
 'use strict';
 
-const fs = require('fs');
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
+const fs = require('fs');
 
 let inputString = '';
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
+process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
     main();
 });
@@ -22,28 +23,30 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-function diagonalDifference(arr) {
-    let ans = 0
-    for(let i = 0; i < arr.length; i++) {
-        ans += arr[i][i] - arr[i][arr.length - i - 1];
-    }
-    return Math.abs(ans);
+function checkPositive(e){
+    return e > 0;
+}
+
+function checkNegative(e){
+    return e < 0;
+}
+
+function checkZero(e){
+    return e == 0;
+}
+
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    ws.write((arr.filter(checkPositive).length / arr.length).toFixed(6) + '\n');
+    ws.write((arr.filter(checkNegative).length / arr.length).toFixed(6) + '\n');
+    ws.write((arr.filter(checkZero).length / arr.length).toFixed(6) + '\n');
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const n = parseInt(readLine(), 10);
 
-    const n = parseInt(readLine().trim(), 10);
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
 
-    let arr = Array(n);
-
-    for (let i = 0; i < n; i++) {
-        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    }
-
-    const result = diagonalDifference(arr);
-
-    ws.write(result + '\n');
-
-    ws.end();
+    plusMinus(arr);
 }
