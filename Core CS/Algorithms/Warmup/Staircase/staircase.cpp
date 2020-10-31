1f6c7a987ed8ea5037aed0f61db10d096b81f543
Marco D <domandominguez@hotmail.com>
2020-10-30 21:27:53 -0700

    rename code.cpp

--- Core CS/Algorithms/Warmup/Staircase/staircase.cpp
+++ Core CS/Algorithms/Warmup/Staircase/staircase.cpp
@@ -1,13 +1,7 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int n = 0;
     cin>>n;
     for(int i = 0;i<n;i++){
