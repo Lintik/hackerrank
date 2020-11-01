f364de770b2be5c5b4c9347aa21ecb2a28ff2f53
Marco D <domandominguez@hotmail.com>
2020-10-31 20:40:13 -0700

    update miniMaxSum.cpp

--- Core CS/Algorithms/Warmup/Mini-Max Sum/miniMaxSum.cpp
+++ Core CS/Algorithms/Warmup/Mini-Max Sum/miniMaxSum.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
-typedef long long LL;
 using namespace std;
 
 int main(){
-	LL s[5];
-	LL d = 0;
-	for(int i = 0; i < 5; i++){
-		cin >> s[i];
-		d += s[i];
-	}
-	sort(s,s+5);
-	cout << d-s[4] << " " << d-s[0] << endl;
+    long long s[5];
+    long long d = 0;
+    for(int i = 0; i < 5; i++){
+        cin >> s[i];
+        d += s[i];
+    }
+    sort(s,s+5);
+    cout << d-s[4] << " " << d-s[0] << endl;
 }
