2f5d6f5426eb87e9dcd6057e4b5d0dd01b0428d0
Marco D <domandominguez@hotmail.com>
2020-11-02 18:30:02 -0800

    update birthdayCakeCandles.scala

---
+++ Core CS/Algorithms/Warmup/Birthday Cake Candles/birthdayCakeCandles.scala
@@ -0,0 +1,29 @@
+import java.io._
+import java.text._
+import java.util._
+import scala.io._
+
+
+object Result {
+
+    def birthdayCakeCandles(candles: Array[Int]): Int = {
+        return candles.filter(_ == candles.max).length
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val candlesCount = StdIn.readLine.trim.toInt
+
+        val candles = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val result = Result.birthdayCakeCandles(candles)
+
+        printWriter.println(result)
+
+        printWriter.close()
+    }
+}
