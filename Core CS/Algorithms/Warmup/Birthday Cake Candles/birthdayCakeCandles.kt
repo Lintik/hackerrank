4fed5945afedf520620ce4d3da1eb6cee54e501f
Marco D <domandominguez@hotmail.com>
2020-11-02 18:22:46 -0800

    update birthdayCakeCandles.kt

---
+++ Core CS/Algorithms/Warmup/Birthday Cake Candles/birthdayCakeCandles.kt
@@ -0,0 +1,18 @@
+import java.io.*
+import java.text.*
+import java.util.*
+import kotlin.collections.*
+
+fun birthdayCakeCandles(candles: Array<Int>): Int {
+    return candles.filter{it == candles.max()}.size
+}
+
+fun main(args: Array<String>) {
+    val candlesCount = readLine()!!.trim().toInt()
+
+    val candles = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(candles)
+
+    println(result)
+}
