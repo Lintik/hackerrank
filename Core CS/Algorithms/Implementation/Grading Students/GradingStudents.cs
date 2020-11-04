e327dd8b8146ec8565197b6f93f32e9ffcd31f09
Marco D <domandominguez@hotmail.com>
2020-11-03 18:46:37 -0800

    update GradingStudents.cs

--- Core CS/Algorithms/Implementation/Grading Students/GradingStudents.cs
+++ Core CS/Algorithms/Implementation/Grading Students/GradingStudents.cs
@@ -4,8 +4,7 @@ using System.IO;
 using System.Linq;
 class Solution {
 
-    static int[] solve(int[] grades){
-        // Complete this function
+    static int[] gradingStudents(int[] grades){
         for(int i=0;i<grades.Length;i++){
         if(grades[i]>=38 && grades[i]%5>2)
             grades[i] = grades[i] - grades[i]%5 + 5;
@@ -19,7 +18,7 @@ class Solution {
         for(int grades_i = 0; grades_i < n; grades_i++){
            grades[grades_i] = Convert.ToInt32(Console.ReadLine());   
         }
-        int[] result = solve(grades);
+        int[] result = gradingStudents(grades);
         Console.WriteLine(String.Join("\n", result));
         
 
