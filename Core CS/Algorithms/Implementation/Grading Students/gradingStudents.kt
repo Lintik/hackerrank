79c4012d5b0300b7aec9a5a820208561153c993b
Marco D <domandominguez@hotmail.com>
2020-11-03 18:35:09 -0800

    update code

--- Core CS/Algorithms/Implementation/Grading Students/gradingStudents.kt
+++ Core CS/Algorithms/Implementation/Grading Students/gradingStudents.kt
@@ -1,33 +1,10 @@
 import java.io.*
-import java.math.*
-import java.security.*
 import java.text.*
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
 import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
-
-/*
- * Complete the 'gradingStudents' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY grades as parameter.
- */
 
 fun gradingStudents(grades: Array<Int>): Array<Int> {
-    // Write your code here
-        return grades.map({g -> if (g < 38 || g % 5 < 3) g else g + 5 - (g % 5)}).toTypedArray()
-
+    return grades.map({g -> if (g < 38 || g % 5 < 3) g else g + 5 - (g % 5)}).toTypedArray()
 }
 
 fun main(args: Array<String>) {
