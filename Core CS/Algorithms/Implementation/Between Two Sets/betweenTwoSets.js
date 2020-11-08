4adeb9efcc66b6f02877828a99122245a2c8130f
Marco D <domandominguez@hotmail.com>
2020-11-08 00:34:06 -0800

    update betweenTwoSets.js

--- Core CS/Algorithms/Implementation/Between Two Sets/betweenTwoSets.js
+++ Core CS/Algorithms/Implementation/Between Two Sets/betweenTwoSets.js
@@ -22,18 +22,59 @@ function readLine() {
     return inputString[currentLine++];
 }
 
+function gcd(a, b){
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+}
 
-function getTotalX(a, b) {
+function gcd(a){
+    let ans = a[0];
+    for (let i = 1; i < a.length; i++) {
+        ans = gcd(ans, a[i]);
+    }
+    return ans;
+}
 
+function lcm(a, b){
+    return a * (b / gcd(a, b));
+}
+
+function lcm(a){
+    let ans = a[0]; 
+
+    for (let i = 1; i < a.length; i++) 
+        ans = (((a[i] * ans)) / 
+                (gcd(a[i], ans))); 
+
+    return ans; 
+}
+
+function getTotalX(arr, brr) {
+    let l = lcm(arr);
+    let g = gcd(brr);
+    
+    let count = 0;
+    for(let i = l, j =2; i<=g; i=l*j,j++){
+        if(g%i==0)
+            count++;
+    }
+    return count;
 }
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
     const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
     const n = parseInt(firstMultipleInput[0], 10);
+
     const m = parseInt(firstMultipleInput[1], 10);
+
     const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
     const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
     const total = getTotalX(arr, brr);
 
     ws.write(total + '\n');
