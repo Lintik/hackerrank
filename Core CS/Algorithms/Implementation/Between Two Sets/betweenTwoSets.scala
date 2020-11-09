c5cc9a55c5a1876aea966ba5ec9f6b9d764203b1
Marco D <domandominguez@hotmail.com>
2020-11-09 12:58:57 -0800

    update betweenTwoSets.scala

---
+++ Core CS/Algorithms/Implementation/Between Two Sets/betweenTwoSets.scala
@@ -0,0 +1,68 @@
+import java.io._
+import java.text._
+import java.util._
+
+object Result {
+    
+    def gcd(a: Int, b: Int): Int{
+        if (b == 0) 
+            return a; 
+        return gcd(b, a % b); 
+    }
+
+    def gcd(a: Array[Int]): Int{
+        var ans = a[0];
+        for(i in 1 until a.size)
+            ans = gcd(ans, a[i])
+        return ans;
+    }
+
+    def lcm(a: Int, b: Int): Int{
+        return a * (b / gcd(a, b));
+    }
+
+    def lcm(a: Array[Int]): Int{
+        var ans = a[0]; 
+    
+        for (i in 1 until a.size) 
+            ans = (((a[i] * ans)) / (gcd(a[i], ans))); 
+    
+        return ans; 
+    }
+
+
+    def getTotalX(a: Array[Int], b: Array[Int]): Int = {
+        var l = lcm(a)
+        var g = gcd(b)
+        
+        var count = 0
+        for(int i = l, j =2; i<=g; i=l*j,j++){
+            if(g%i==0)
+                count++
+        }
+        return count
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val firstMultipleInput = StdIn.readLine.replaceAll("\\s+$", "").split(" ")
+
+        val n = firstMultipleInput(0).toInt
+
+        val m = firstMultipleInput(1).toInt
+
+        val arr = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val brr = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val total = Result.getTotalX(arr, brr)
+
+        printWriter.println(total)
+
+        printWriter.close()
+    }
+}
