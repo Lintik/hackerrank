ca060016d35d851b88c1d7c58d38e7d5dca9432a
Marco D <domandominguez@hotmail.com>
2020-11-10 14:08:19 -0800

    update breakingTheRecords.scala

--- Core CS/Algorithms/Implementation/Breaking the Records/breakingTheRecords.scala
+++ Core CS/Algorithms/Implementation/Breaking the Records/breakingTheRecords.scala
@@ -5,8 +5,20 @@ import java.util._
 object Solution {
 
     def breakingRecords(scores: Array[Int]): Array[Int] = {
-
-
+        var maxc, minc = 0
+        var m, n = scores(0)
+        
+        for(i <- 1 until scores.size)
+            if (m < scores(i)){
+                m = scores(i)
+                maxc += 1
+            }
+            else if (n > scores(i)){
+                n = scores(i)
+                minc += 1
+            }
+        
+        return Array(maxc,minc)
     }
 
     def main(args: Array[String]) {
