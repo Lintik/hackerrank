eac7db09ad623fd2bedb20624344dc01bb0d4c54
Marco D <domandominguez@hotmail.com>
2020-11-13 20:50:46 -0800

    update billDivision.js

---
+++ Core CS/Algorithms/Implementation/Bill Division/billDivision.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function bonAppetit(bill, k, b) {
+    let s = bill.reduce(function(a,b){
+        return a + b;
+    }) - bill[k];
+    
+        
+    if(b - s/2 == 0)
+         console.log("Bon Appetit");
+    else
+         console.log((b - s/2).toString());
+
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
