17cd2ca7869184f278225fd57e90a2fb80e74df1
Marco D <domandominguez@hotmail.com>
2020-11-13 21:12:00 -0800

    update Sales By Match

--- Core CS/Algorithms/Implementation/Sales by Match/salesByMatch.cpp
+++ Core CS/Algorithms/Implementation/Sales by Match/salesByMatch.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int sockMerchant(int n, vector <int> ar) {
--- Core CS/Algorithms/Implementation/Sales by Match/salesByMatch.java
+++ Core CS/Algorithms/Implementation/Sales by Match/salesByMatch.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
